91c38dc6a1fccd41d63dedcb3c74175938345a57
Kuma-Boo <booleanmetagame@gmail.com>
2023-08-16 14:55:07 -0400

    Refactored EventTrigger.cs
    Fixed sandfall event exit collisions
    Improved EventTrigger.cs inspector editing
    Cleaned up animations slightly

--- Project/object/trigger/modules/EventTrigger.cs
+++ Project/object/trigger/modules/EventTrigger.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 
 namespace Project.Gameplay.Triggers
 {
@@ -6,14 +7,14 @@ namespace Project.Gameplay.Triggers
 	/// Triggers environment effects/cutscenes.
 	/// For gameplay automated sections (such as loops), see <see cref="AutomationTrigger"/>.
 	/// </summary>
+	[Tool]
 	public partial class EventTrigger : StageTriggerModule
 	{
 		[Signal]
 		public delegate void ActivatedEventHandler();
 
-		[Export]
+		/// <summary> Automatically reset the event when player respawns? </summary>
 		private bool autoRespawn;
-		[Export]
 		/// <summary> Only allow event to play once? </summary>
 		private bool isOneShot = true;
 		private bool wasActivated;
@@ -21,21 +22,126 @@ namespace Project.Gameplay.Triggers
 		[ExportGroup("Components")]
 		[Export]
 		private AnimationPlayer animator;
-		[Export]
-		private Node3D playerStandin;
-		[Export]
-		private Node3D cameraStandin;
-		[Export]
-		private LockoutResource lockout;
-		[Export(PropertyHint.Range, "0, 1")]
-		private float smoothing = .2f;
 
 		private readonly StringName EVENT_ANIMATION = "event";
 		private readonly StringName RESET_ANIMATION = "RESET";
 
+		#region Editor
+		public override Array<Dictionary> _GetPropertyList()
+		{
+			Array<Dictionary> properties = new Array<Dictionary>();
+
+			properties.Add(ExtensionMethods.CreateProperty("Trigger Settings/Automatically Respawn", Variant.Type.Bool));
+			properties.Add(ExtensionMethods.CreateProperty("Trigger Settings/Is One Shot", Variant.Type.Bool));
+
+			properties.Add(ExtensionMethods.CreateProperty("Trigger Settings/Camera Stand-in", Variant.Type.NodePath));
+			properties.Add(ExtensionMethods.CreateProperty("Trigger Settings/Player Stand-in", Variant.Type.NodePath));
+
+
+			if (!playerStandin.IsEmpty) // Add player event settings
+			{
+				properties.Add(ExtensionMethods.CreateProperty("Player Event Settings/Animation", Variant.Type.StringName));
+				properties.Add(ExtensionMethods.CreateProperty("Player Event Settings/Animation Fadeout Time", Variant.Type.Float));
+				properties.Add(ExtensionMethods.CreateProperty("Player Event Settings/Position Smoothing", Variant.Type.Float, PropertyHint.Range, "0,1,.1"));
+
+				properties.Add(ExtensionMethods.CreateProperty("Player Event Settings/Exit On Ground", Variant.Type.Bool));
+				properties.Add(ExtensionMethods.CreateProperty("Player Event Settings/Normalize Exit Move Speed", Variant.Type.Bool));
+				properties.Add(ExtensionMethods.CreateProperty("Player Event Settings/Exit Move Speed", Variant.Type.Float));
+				properties.Add(ExtensionMethods.CreateProperty("Player Event Settings/Exit Vertical Speed", Variant.Type.Float));
+				properties.Add(ExtensionMethods.CreateProperty("Player Event Settings/Exit Lockout", Variant.Type.Object));
+			}
+
+			return properties;
+		}
+
+		public override Variant _Get(StringName property)
+		{
+			switch ((string)property)
+			{
+				case "Trigger Settings/Automatically Respawn":
+					return autoRespawn;
+				case "Trigger Settings/Is One Shot":
+					return isOneShot;
+				case "Trigger Settings/Camera Stand-in":
+					return cameraStandin;
+				case "Trigger Settings/Player Stand-in":
+					return playerStandin;
+
+				case "Player Event Settings/Animation":
+					return characterAnimation;
+				case "Player Event Settings/Animation Fadeout Time":
+					return characterFadeoutTime;
+				case "Player Event Settings/Position Smoothing":
+					return characterPositionSmoothing;
+
+				case "Player Event Settings/Normalize Exit Move Speed":
+					return normalizeExitMoveSpeed;
+				case "Player Event Settings/Exit Move Speed":
+					return characterExitMoveSpeed;
+				case "Player Event Settings/Exit Vertical Speed":
+					return characterExitVerticalSpeed;
+				case "Player Event Settings/Exit Lockout":
+					return characterExitLockout;
+			}
+
+			return base._Get(property);
+		}
+
+		public override bool _Set(StringName property, Variant value)
+		{
+			switch ((string)property)
+			{
+				case "Trigger Settings/Automatically Respawn":
+					autoRespawn = (bool)value;
+					break;
+				case "Trigger Settings/Is One Shot":
+					isOneShot = (bool)value;
+					break;
+				case "Trigger Settings/Camera Stand-in":
+					cameraStandin = (NodePath)value;
+					break;
+				case "Trigger Settings/Player Stand-in":
+					playerStandin = (NodePath)value;
+					NotifyPropertyListChanged();
+					break;
+
+				case "Player Event Settings/Animation":
+					characterAnimation = new StringName((string)value);
+					break;
+				case "Player Event Settings/Animation Fadeout Time":
+					characterFadeoutTime = (float)value;
+					break;
+				case "Player Event Settings/Position Smoothing":
+					characterPositionSmoothing = (float)value;
+					break;
+
+				case "Player Event Settings/Normalize Exit Move Speed":
+					normalizeExitMoveSpeed = (bool)value;
+					NotifyPropertyListChanged();
+					break;
+				case "Player Event Settings/Exit Move Speed":
+					characterExitMoveSpeed = (float)value;
+					break;
+				case "Player Event Settings/Exit Vertical Speed":
+					characterExitVerticalSpeed = (float)value;
+					break;
+				case "Player Event Settings/Exit Lockout":
+					characterExitLockout = (LockoutResource)value;
+					break;
+
+				default:
+					return false;
+			}
+
+			return true;
+		}
+		#endregion
+
 
 		public override void _Ready()
 		{
+			if (Engine.IsEditorHint()) return;
+
 			if (autoRespawn)
 				StageSettings.instance.ConnectRespawnSignal(this);
 		}
@@ -43,6 +149,8 @@ namespace Project.Gameplay.Triggers
 
 		public override void _PhysicsProcess(double _)
 		{
+			if (Engine.IsEditorHint()) return;
+
 			if (playerStandin == null)
 				return;
 
@@ -82,29 +190,35 @@ namespace Project.Gameplay.Triggers
 			animator.Play(EVENT_ANIMATION);
 
 
-			if (playerStandin != null)
+			if (!playerStandin.IsEmpty)
 			{
-				Character.StartExternal(this, playerStandin, smoothing);
+				Character.StartExternal(this, GetNode<Node3D>(playerStandin), characterPositionSmoothing);
 				Character.Animator.ExternalAngle = 0; // Reset external angle
 				Character.Animator.SnapRotation(Character.Animator.ExternalAngle);
+				Character.Animator.PlayOneshotAnimation(characterAnimation);
 			}
 
-			if (cameraStandin != null) // Set external camera
-				Character.Camera.SetExternalController(cameraStandin);
+			if (!cameraStandin.IsEmpty) // Set external camera
+				Character.Camera.SetExternalController(GetNode<Node3D>(cameraStandin));
 		}
 
 
-		#region Animator Helper Functions
-		/// <summary> Plays a specific oneshot animation on the player. </summary>
-		public void PlayCharacterAnimation(StringName animationName) => Character.Animator.PlayOneshotAnimation(animationName);
+		#region Event Animation
+		private NodePath playerStandin;
+		private NodePath cameraStandin;
+		/// <summary> Lockout to apply when character finishes event. </summary>
+		private LockoutResource characterExitLockout;
+		private float characterPositionSmoothing = .2f;
 
+		/// <summary> How much to fadeout character's animation by. </summary>
+		private float characterFadeoutTime;
+		/// <summary> Which event animation to play on the character. </summary>
+		private StringName characterAnimation;
 
-		/// <summary> Sets the character's speeds. </summary>
-		public void SetCharacterSpeed(float moveSpeed, float verticalSpeed)
-		{
-			Character.MoveSpeed = moveSpeed;
-			Character.VerticalSpeed = verticalSpeed;
-		}
+		/// <summary> Evaluate exit move speed as a ratio instead of a default value. </summary>
+		private bool normalizeExitMoveSpeed = true;
+		private float characterExitMoveSpeed;
+		private float characterExitVerticalSpeed;
 
 
 		/// <summary> Resets the character's movement state. </summary>
@@ -112,15 +226,18 @@ namespace Project.Gameplay.Triggers
 		{
 			Character.ResetMovementState();
 
-			Character.MovementAngle = ExtensionMethods.CalculateForwardAngle(playerStandin.Forward());
+			Character.MovementAngle = ExtensionMethods.CalculateForwardAngle(Character.ExternalParent.Forward());
 			Character.Animator.SnapRotation(Character.MovementAngle);
 			Character.Animator.CancelOneshot(fadeout);
 
-			if (lockout != null)
-				Character.AddLockoutData(lockout);
+			if (characterExitLockout != null)
+				Character.AddLockoutData(characterExitLockout);
 
 			if (cameraStandin != null) // Remove external camera
 				Character.Camera.SetExternalController(null);
+
+			Character.MoveSpeed = normalizeExitMoveSpeed ? Character.GroundSettings.speed * characterExitMoveSpeed : characterExitMoveSpeed;
+			Character.VerticalSpeed = characterExitVerticalSpeed;
 		}
 		#endregion
 	}
