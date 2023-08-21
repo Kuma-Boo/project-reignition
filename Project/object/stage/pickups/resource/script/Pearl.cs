5b533a7fca8bd03dd8e1b54cbf2d6daf8b1bf895
Kuma-Boo <booleanmetagame@gmail.com>
2023-08-21 19:24:35 -0400

    Updated pearls
    Alter pearl collection tween to give the player more space

--- Project/object/stage/pickups/resource/script/Pearl.cs
+++ Project/object/stage/pickups/resource/script/Pearl.cs
@@ -43,17 +43,21 @@ namespace Project.Gameplay.Objects
 			if (tweener != null)
 				tweener.Kill();
 
+			// Collection tween
 			tweener = CreateTween().SetTrans(Tween.TransitionType.Sine);
-			//Collection tween
-			bool reverseDirection = Mathf.Sign(Character.Forward().Dot(Position)) < 0; //True when collecting a pearl behind us
+			float distance = Runtime.randomNumberGenerator.RandfRange(.4f, .8f);
+			Vector3 endPoint = new Vector3(distance, .5f, -.5f);
+			Vector3 midPoint = new Vector3(distance * 2, (Position.Y + endPoint.Y) * .5f, -.25f);
 
-			Vector3 endPoint = new Vector3(0, .5f, 0);
-			Vector3 midPoint = new Vector3(Runtime.randomNumberGenerator.Randfn(0, .4f), (Position.Y + endPoint.Y) * .5f, 0);
+			if (Runtime.randomNumberGenerator.Randf() > .5f)
+			{
+				endPoint.X *= -1;
+				midPoint.X *= -1;
+			}
 
-			if (reverseDirection)
+			if (Character.IsMovingBackward)
 			{
 				endPoint.Z *= -1;
-				midPoint.X *= -1;
 				midPoint.Z *= -1;
 			}
 
@@ -61,14 +65,13 @@ namespace Project.Gameplay.Objects
 			tweener.TweenProperty(this, "position", endPoint, .2f).SetEase(Tween.EaseType.In);
 			tweener.Parallel().TweenProperty(this, "scale", Vector3.One * .001f, .2f).SetEase(Tween.EaseType.In);
 
-			//TODO Modify soul gauge
 			tweener.TweenCallback(Callable.From(() => Character.Skills.ModifySoulGauge(isRichPearl ? 20 : 1))).SetDelay(.1f);
 			tweener.TweenCallback(new Callable(this, MethodName.Despawn)).SetDelay(3f);
 
-			if (!isRichPearl) //Play the correct sfx
-				Sound.PlayPearlSFX();
-			else
+			if (isRichPearl) // Play the correct sfx
 				Sound.PlayRichPearlSFX();
+			else
+				Sound.PlayPearlSFX();
 
 			StageSettings.instance.UpdateScore(isRichPearl ? 5 : 1, StageSettings.MathModeEnum.Add);
 			isCollected = true;
