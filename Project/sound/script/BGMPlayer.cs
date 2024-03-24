aa26b9283b3f1f092fec7845c3cae5cc625b1fb7
Kuma-Boo <booleanmetagame@gmail.com>
2024-03-24 11:37:48 -0400

    Fix level complete music looping
    Reformatted BGMPlayer.cs

--- Project/sound/script/BGMPlayer.cs
+++ Project/sound/script/BGMPlayer.cs
@@ -25,7 +25,7 @@ namespace Project
 				stageMusicInstance.VolumeDb = db;
 		}
 
-		public static void StartStageMusic() //Called when countdown starts to keep things in sync, regardless of load times.
+		public static void StartStageMusic() // Called when countdown starts to keep things in sync, regardless of load times.
 		{
 			if (stageMusicInstance != null)
 				stageMusicInstance.Play();
@@ -36,12 +36,13 @@ namespace Project
 		[Export]
 		public float loopEndPosition;
 		[Export]
-		public float debugSeek; //Editor debug. Seeks to the specified point (in seconds)
+		public float debugSeek = -1; // Editor debug. Seeks to the specified point (in seconds)
 		[Export]
 		public bool isStageMusic;
 
 		private bool canLoop;
 		private float LoopLength => loopEndPosition - loopStartPosition;
+
 
 		public override void _EnterTree()
 		{
@@ -49,16 +50,18 @@ namespace Project
 			if (!canLoop)
 				GD.PrintErr("BGM loop points are set up incorrectly. Looping is disabled.");
 
-			//Only one stage music can be playing at a time
+			// Only one stage music can be playing at a time
 			if (isStageMusic)
 				stageMusicInstance = this;
 		}
 
+
 		public override void _ExitTree()
 		{
-			if (stageMusicInstance == this) //Unreference
+			if (stageMusicInstance == this) // Unreference
 				stageMusicInstance = null;
 		}
+
 
 		public override void _Process(double _)
 		{
@@ -69,18 +72,20 @@ namespace Project
 			if (currentPosition >= loopEndPosition)
 				Seek(currentPosition - LoopLength);
 
-			if (Engine.IsEditorHint() && Mathf.IsEqualApprox(debugSeek, -1))
+			if (Engine.IsEditorHint() && !Mathf.IsEqualApprox(debugSeek, -1))
 			{
 				Seek(debugSeek);
 				debugSeek = -1;
 			}
 		}
 
+
 		public void RestartLoop()
 		{
 			if (GetPlaybackPosition() >= loopEndPosition)
 				Play(loopStartPosition);
 		}
+
 
 		public void Play() => Play(0);
 	}
