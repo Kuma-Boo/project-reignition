cf0f7b546d1ea0f56d2569d5b8e0a4686b02d191
Kuma-Boo <46894189+Kuma-Boo@users.noreply.github.com>
2024-04-12 23:39:35 -0400

    Update Pause Menu (WIP)
    Update font
    Update flame sprites
    Update localization

--- Project/interface/gameplay/pause/PauseMenu.cs
+++ Project/interface/gameplay/pause/PauseMenu.cs
@@ -6,55 +6,20 @@ namespace Project.Interface
 	public partial class PauseMenu : Node
 	{
 		[Export]
-		private AnimationPlayer pauseAnimator;
-
-		private bool canInteractWithPauseMenu = true;
-
-		private void EnableInteraction() => canInteractWithPauseMenu = true;
-
-		private void TogglePause()
-		{
-			canMoveCursor = false; //Disable cursor movement
-			canInteractWithPauseMenu = false; //Disable pause inputs during the animation
-
-			bool isPaused = !GetTree().Paused;
-
-			GetTree().Paused = isPaused;
-			BGMPlayer.StageMusicPaused = isPaused;
-			pauseAnimator.Play(isPaused ? "pause" : "unpause");
-
-			if (isPaused) //Reset selection
-			{
-				currentSelection = 0;
-				UpdateCursorPosition();
-			}
-		}
-
+		private AnimationMixer pauseAnimator;
 		[Export]
-		private Control cursorNode;
+		private AudioStreamPlayer screenChangeSFX;
+		[Export]
+		private Node2D cursor;
 		[Export]
 		private AnimationPlayer cursorAnimator;
-		/// <summary> Selected menu option. </summary>
-		private int currentSelection;
-		/// <summary> Can the cursor currently be moved? </summary>
-		private bool canMoveCursor;
-		private const int MAX_SELECTION = 3;
-		private const int SELECTION_SPACING = 56;
 
-		private void EnableCursorMovement() => canMoveCursor = true;
-		private void UpdateCursorPosition()
-		{
-			cursorNode.Position = Vector2.Down * currentSelection * SELECTION_SPACING;
-			cursorAnimator.Play("show");
-			cursorAnimator.Seek(0, true);
-		}
+		private bool isActive;
+		private bool canInteractWithPauseMenu = true;
 
-		private void UpdateSelection(int selection)
-		{
-			canMoveCursor = false;
-			cursorAnimator.Play("hide");
-			currentSelection = selection;
-		}
+		private readonly StringName SELECTION_PARAMETER = "parameters/selection/transition_request";
+		private readonly StringName STATE_PARAMETER = "parameters/state/transition_request";
+
 
 		public override void _PhysicsProcess(double delta)
 		{
@@ -89,9 +54,59 @@ namespace Project.Interface
 			}
 		}
 
+
+		/// <summary> Selected menu option. </summary>
+		private int currentSelection;
+		/// <summary> Can the cursor currently be moved? </summary>
+		private bool canMoveCursor;
+		private const int MAX_SELECTION = 3;
+		private const int SELECTION_SPACING = 32;
+		private void EnableCursorMovement() => canMoveCursor = true;
+		private void UpdateCursorPosition()
+		{
+			cursor.Position = Vector2.Down * currentSelection * SELECTION_SPACING;
+			cursorAnimator.Play("show");
+			cursorAnimator.Seek(0, true);
+		}
+
+
+		private void UpdateSelection(int selection)
+		{
+			canMoveCursor = false;
+			cursorAnimator.Play("hide");
+			currentSelection = selection;
+			pauseAnimator.Set(SELECTION_PARAMETER, selection.ToString());
+		}
+
+
+		private void EnableInteraction() => canInteractWithPauseMenu = true;
+		private void TogglePause()
+		{
+			canMoveCursor = false; //Disable cursor movement
+			canInteractWithPauseMenu = false; //Disable pause inputs during the animation
+
+			isActive = !isActive;
+			pauseAnimator.Set(STATE_PARAMETER, isActive ? "show" : "hide");
+
+			if (isActive) //Reset selection
+			{
+				currentSelection = 0;
+				UpdateCursorPosition();
+				pauseAnimator.Set(SELECTION_PARAMETER, "show");
+			}
+		}
+
+
+		private void ApplyPause()
+		{
+			GetTree().Paused = isActive;
+			BGMPlayer.StageMusicPaused = isActive;
+		}
+
+
 		private void StartSceneTransition(string targetScene)
 		{
-			pauseAnimator.Play("screen-change");
+			screenChangeSFX.Play();
 
 			canMoveCursor = false;
 			canInteractWithPauseMenu = false;
