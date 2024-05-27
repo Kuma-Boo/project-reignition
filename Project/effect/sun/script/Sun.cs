c1ed6a01e5878792644957bceeddaf1a7681777f
Kuma-Boo <46894189+Kuma-Boo@users.noreply.github.com>
2024-05-26 21:53:00 -0400

    Fix sun

--- Project/effect/sun/script/Sun.cs
+++ Project/effect/sun/script/Sun.cs
@@ -9,7 +9,7 @@ namespace Project
 	{
 		[Export]
 		private Node2D lensFlareBase;
-		private readonly Array<Node2D> _lensPieces = new Array<Node2D>();
+		private readonly Array<Node2D> _lensPieces = new();
 		private readonly float LENS_FLARE_SPACING = .2f;
 
 		[Export(PropertyHint.Range, "0,1,0.1")]
@@ -21,14 +21,14 @@ namespace Project
 		private Vector2 screenUV;
 		private float currentMovement;
 		private float currentMovementVelocity;
-		private bool isOccluded = true; //Start occluded
+		private bool isOccluded = true; // Start occluded
 		private float currentOcclusion;
 		private float currentOcclusionVelocity;
 		private readonly float OCCLUSION_SMOOTHING = .5f;
 		private readonly float MOVEMENT_SMOOTHING = 4.0f;
 
 		private CameraController Camera => CharacterController.instance.Camera;
-		private Callable UpdateSunCallable => new Callable(this, MethodName.UpdateSun);
+		private Callable UpdateSunCallable => new(this, MethodName.UpdateSun);
 
 		private readonly StringName SHADER_GLOBAL_SUN_OCCLUSION = "sun_occlusion";
 		private readonly StringName SHADER_GLOBAL_SUN_MOVEMENT = "sun_movement";
@@ -50,7 +50,7 @@ namespace Project
 
 		private void UpdateSun()
 		{
-			if (Camera == null || DepthRenderer.DepthTexture == null) return; //No camera/depth texture found
+			if (Camera == null || DepthRenderer.DepthTexture == null) return; // No camera/depth texture found
 
 			currentOcclusion = ExtensionMethods.SmoothDamp(currentOcclusion, isOccluded ? 1f : 0f, ref currentOcclusionVelocity, OCCLUSION_SMOOTHING);
 			RenderingServer.GlobalShaderParameterSet(SHADER_GLOBAL_SUN_OCCLUSION, currentOcclusion);
@@ -67,24 +67,24 @@ namespace Project
 		}
 
 		private float updateTimer;
-		private readonly float UPDATE_INTERVAL = .5f; //How often to update the sun. Reduces frame drops
+		private readonly float UPDATE_INTERVAL = .5f; // How often to update the sun. Reduces frame drops
 		private void UpdateSample()
 		{
 			if (updateTimer <= 0f)
 			{
 				if (Camera.IsBehindCamera(GlobalPosition) || screenUV.X < 0.0f || screenUV.X > 1.0f || screenUV.Y < 0.0f || screenUV.Y > 1.0f)
-					isOccluded = true; //Always occluded when the camera faces away
+					isOccluded = true; // Always occluded when the camera faces away
 				else if (Camera.IsOnScreen(GlobalPosition))
 				{
-					//Sample texture. VERY SLOW!!!
+					// Sample texture. VERY SLOW!!!
 					Image depthBuffer = DepthRenderer.DepthTexture.GetImage();
 					Vector2I samplePosition = (Vector2I)(screenUV * depthBuffer.GetSize());
 					samplePosition = samplePosition.Clamp(Vector2I.Zero, depthBuffer.GetSize() - Vector2I.One);
 
-					//Since the sun is so far away, a simple alpha check can determine occlusion.
+					// Since the sun is so far away, a simple alpha check can determine occlusion.
 					float alpha = depthBuffer.GetPixelv(samplePosition).A;
 					isOccluded = !Mathf.IsZeroApprox(alpha);
-					depthBuffer.Dispose(); //Don't forget to dispose the image!
+					depthBuffer.Dispose(); // Don't forget to dispose the image!
 				}
 
 				updateTimer = UPDATE_INTERVAL;
@@ -96,7 +96,7 @@ namespace Project
 		private void UpdateLensFlare()
 		{
 			Vector2 originPosition = Camera.ConvertToScreenSpace(GlobalPosition);
-			Vector2 flareDirection = lensFlareBase.GlobalPosition - originPosition; //Get the direction to the center of the screen
+			Vector2 flareDirection = lensFlareBase.GlobalPosition - originPosition; // Get the direction to the center of the screen
 
 			lensFlareBase.Modulate = Colors.White.Lerp(Colors.Transparent, currentOcclusion);
 			for (int i = 0; i < _lensPieces.Count; i++)
