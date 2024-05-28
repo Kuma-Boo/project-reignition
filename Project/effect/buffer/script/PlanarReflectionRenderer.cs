43cf6e5fb1614e2248971520f4209a1a12ab4f45
Kuma-Boo <46894189+Kuma-Boo@users.noreply.github.com>
2024-05-28 10:48:05 -0400

    Simplify Reflection Renderer

--- Project/effect/buffer/script/PlanarReflectionRenderer.cs
+++ Project/effect/buffer/script/PlanarReflectionRenderer.cs
@@ -4,39 +4,29 @@ using Project.Core;
 
 namespace Project.Gameplay
 {
-	/// <summary> Renders planar reflections into a global texture, then updates materials in reflectionMaterials. </summary>
+	/// <summary>
+	/// Renders planar reflections into a global texture, then updates materials in reflectionMaterials.
+	/// To have multiple reflections at different elevations, create multiple PlanarReflectionRenderers
+	/// and use separate materials for each.
+	/// </summary>
 	[Tool]
 	public partial class PlanarReflectionRenderer : Node3D
 	{
 		public static PlanarReflectionRenderer instance;
 
-		public static ViewportTexture ReflectionTexture { get; private set; }
-		public static readonly StringName REFLECTION_PARAMETER = "reflection_texture"; //All shaders that use reflections must have this parameter
-
-		/// <summary> Set this if you need more accurate reflection positions. </summary>
-		public Path3D ReflectionSyncPath { get; set; }
-
-		[Export(PropertyHint.Layers3DRender)]
-		private uint renderMask;
+		// All shaders that use reflections must have this parameter
+		public static readonly StringName REFLECTION_PARAMETER = "reflection_texture";
 
 		[Export]
-		private NodePath reflectionCameraPath;
 		private Camera3D reflectionCamera;
-		private Camera3D mainCamera;
 		[Export]
-		private NodePath reflectionViewportPath;
 		private SubViewport reflectionViewport;
-		[Export]
-		private float nearClip = .05f;
+		private Camera3D GameplayCamera => CharacterController.instance.Camera.Camera;
 
 
 		[Export]
 		/// <summary> List of materials that use reflection_texture. </summary>
 		public Array<ShaderMaterial> reflectionMaterials;
-
-		[Export]
-		private Vector3 previousCapturePosition;
-		private Vector3 previousCaptureRotation;
 
 		private Callable UpdatePositionCallable => new(this, MethodName.UpdatePosition);
 		private Callable ApplyTextureCallable => new(this, MethodName.ApplyTexture);
@@ -61,81 +51,48 @@ namespace Project.Gameplay
 				RenderingServer.Singleton.Disconnect(RenderingServer.SignalName.FramePostDraw, ApplyTextureCallable);
 
 			instance = null;
-			ReflectionTexture = null;
 			ApplyTexture();
 		}
 
-		private bool GetCamera()
-		{
-			if (reflectionCamera == null)
-				reflectionCamera = GetNodeOrNull<Camera3D>(reflectionCameraPath);
-
-			if (CharacterController.instance != null)
-				mainCamera = CharacterController.instance.Camera.Camera;
-
-			return mainCamera != null && reflectionCamera != null;
-		}
 
 		//Mirror main camera along plane
 		private void UpdatePosition()
 		{
-			if (Engine.IsEditorHint()) return;
-			if (!GetCamera() || !GetReflectionViewport()) return;
+			reflectionCamera.CullMask = GameplayCamera.CullMask;
+			reflectionCamera.Fov = GameplayCamera.Fov;
+			reflectionCamera.Size = GameplayCamera.Size;
+			reflectionCamera.Projection = GameplayCamera.Projection;
 
-			if (Engine.IsEditorHint())
-			{
-				if (mainCamera.GlobalPosition.IsEqualApprox(previousCapturePosition) &&
-				mainCamera.Forward().IsEqualApprox(previousCaptureRotation)) return; //Didn't move
-				previousCapturePosition = mainCamera.GlobalPosition;
-				previousCaptureRotation = mainCamera.Forward();
-			}
-
-			reflectionCamera.CullMask = renderMask;
-			reflectionCamera.Fov = mainCamera.Fov;
-			reflectionCamera.Size = mainCamera.Size;
-			reflectionCamera.Projection = mainCamera.Projection;
-
-			reflectionCamera.Near = nearClip;
-			reflectionCamera.Far = mainCamera.Far;
+			reflectionCamera.Near = GameplayCamera.Near;
+			reflectionCamera.Far = GameplayCamera.Far;
 
 			// Calculate reflection position
 			Vector3 reflectionPosition = GlobalPosition;
-			if (IsInstanceValid(ReflectionSyncPath)) // Use reflection syncing path?
-				reflectionPosition = ReflectionSyncPath.Curve.GetClosestPoint(CharacterController.instance.GlobalPosition - ReflectionSyncPath.GlobalPosition);
 
 			// Update reflectionCamera's position
 			Vector3 reflectionAxis = this.Up();
-			Vector3 projection = reflectionAxis * reflectionAxis.Dot(mainCamera.GlobalPosition - reflectionPosition);
-			Vector3 targetPosition = (mainCamera.GlobalPosition - projection * 2f);
+			Vector3 projection = reflectionAxis * reflectionAxis.Dot(GameplayCamera.GlobalPosition - reflectionPosition);
+			Vector3 targetPosition = (GameplayCamera.GlobalPosition - projection * 2f);
 
 			// Update reflectionCamera's rotation
-			Vector3 upDirection = mainCamera.Up().Reflect(reflectionAxis.Normalized());
-			Vector3 forwardDirection = mainCamera.Forward().Reflect(reflectionAxis.Normalized());
+			Vector3 upDirection = GameplayCamera.Up().Reflect(reflectionAxis.Normalized());
+			Vector3 forwardDirection = GameplayCamera.Forward().Reflect(reflectionAxis.Normalized());
 
 			reflectionCamera.LookAtFromPosition(targetPosition, targetPosition + forwardDirection, upDirection);
 
 			if (Engine.IsEditorHint())
-				reflectionViewport.Size = (Vector2I)mainCamera.GetViewport().GetVisibleRect().Size;
+				reflectionViewport.Size = (Vector2I)GameplayCamera.GetViewport().GetVisibleRect().Size;
 			else
 				reflectionViewport.Size = Runtime.HALF_SCREEN_SIZE;
 
 			reflectionViewport.RenderTargetUpdateMode = SubViewport.UpdateMode.Once;
 		}
 
-		private bool GetReflectionViewport()
-		{
-			if (reflectionViewportPath == null) return false;
-			if (reflectionViewport == null)
-				reflectionViewport = GetNodeOrNull<SubViewport>(reflectionViewportPath);
-
-			return reflectionViewport != null;
-		}
 
 		/// <summary> Applies reflection texture to associated shaders. </summary>
 		private void ApplyTexture()
 		{
-			if (GetReflectionViewport())
-				ReflectionTexture = reflectionViewport.GetTexture();
+			ViewportTexture ReflectionTexture = reflectionViewport.GetTexture();
 
 			if (reflectionMaterials != null)
 			{
