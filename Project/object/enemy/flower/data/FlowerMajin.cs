2ab82984d819afb3070c5f5c0799fb9cb605fe6a
Kuma-Boo <kumapauze@gmail.com>
2024-06-09 17:58:22 -0400

    Re-implement flower majin

--- Project/object/enemy/flower/data/FlowerMajin.cs
+++ Project/object/enemy/flower/data/FlowerMajin.cs
@@ -8,65 +8,106 @@ namespace Project.Gameplay
 	{
 		[ExportGroup("Enemy Settings")]
 		[Export]
-		private bool attackInstantly; //Skip passive phase when activated?
+		/// <summary> Skip passive phase when activated? </summary>
+		private bool attackInstantly;
 		[Export]
-		private float passiveLength; //How long to remain passive
+		/// <summary> How long to remain passive. </summary>
+		private float passiveLength;
 		[Export]
-		private float preAttackLength; //How long to wait before firing seeds after turning aggressive
+		/// <summary> How long to wait before firing seeds after turning aggressive. </summary>
+		private float preAttackLength;
 		[Export]
-		private float postAttackLength; //How long to wait after firing seeds before turning passive
+		/// <summary> How long to wait after firing seeds before turning passive. </summary>
+		private float postAttackLength;
+		/// <summary> How much extra time to wait for when staggered. </summary>
+		private const float STAGGER_LENGTH = .5f;
 
-		[ExportGroup("Animation Settings")]
-		[Export]
-		private bool isOpen; //Can the flower be damaged?
-		[Export]
-		private bool isInHitstun; //Is this enemy being clobbered by a homing attack?
+		/// <summary> Tracks how long FlowerMajin has been in the current state. </summary>
+		private float stateTimer;
+		private State currentState;
+		private enum State
+		{
+			Passive,
+			PreAttack,
+			Attack,
+			PostAttack
+		}
+
 		[Export]
 		private PackedScene seed;
 		private int seedIndex;
 		private const int MAX_SEED_COUNT = 3;
-		private readonly Seed[] seedPool = new Seed[MAX_SEED_COUNT]; //Only three seeds can be spawned at a time.
+		/// <summary> Only three seeds are ever spawned at a time. </summary>
+		private readonly Seed[] seedPool = new Seed[MAX_SEED_COUNT];
+
+		/// <summary> Flower is only considered be damaged while not in a passive state. </summary>
+		private bool IsOpen => currentState != State.Passive;
+		/// <summary> Returns true if the flower's stagger animation is active. </summary>
+		private bool IsStaggered => (bool)animationPlayer.Get(STAGGER_ACTIVE);
+
+		private readonly StringName STATE_TRANSITION = "parameters/state_transition/transition_request";
+		private readonly StringName SHOW_STATE = "show";
+		private readonly StringName PASSIVE_STATE = "passive";
+		private readonly StringName HIDE_STATE = "hide";
+
+		private readonly StringName ATTACK_TRIGGER = "parameters/attack_trigger/request";
+		private readonly StringName STAGGER_TRIGGER = "parameters/stagger_trigger/request";
+		private readonly StringName STAGGER_ACTIVE = "parameters/stagger_trigger/active";
+
+		/// <summary> Reference to AnimationTree's defeat_transition node. Required to change transition fade. </summary>
+		private AnimationNodeTransition defeatTransition;
+		private readonly StringName DEFEAT_TRANSITION = "parameters/defeat_transition/transition_request";
+		private readonly StringName ENABLED_CONSTANT = "enabled";
+		private readonly StringName DISABLED_CONSTANT = "disabled";
+
 
 		protected override void SetUp()
 		{
-			StageSettings.instance.ConnectUnloadSignal(this);
+			animationTree.Active = true;
+			defeatTransition = (animationTree.TreeRoot as AnimationNodeBlendTree).GetNode("defeat_transition") as AnimationNodeTransition;
 
-			for (int i = 0; i < MAX_SEED_COUNT; i++)
-			{
+			for (int i = 0; i < MAX_SEED_COUNT; i++) // Initialize seeds
 				seedPool[i] = seed.Instantiate<Seed>();
-			}
 
 			base.SetUp();
 		}
 
+
 		public override void Unload()
 		{
-			for (int i = 0; i < seedPool.Length; i++) //Clear memory
+			for (int i = 0; i < seedPool.Length; i++) // Clear memory
 				seedPool[i].QueueFree();
 
 			base.Unload();
 		}
 
+
 		protected override void Spawn()
 		{
-			if (attackInstantly && currentState == States.Passive) //Skip passive phase
-				stateTimer = passiveLength;
+			if (attackInstantly && !IsOpen) // Skip passive phase
+				StartAggressiveState();
 
 			base.Spawn();
 		}
+
 
 		public override void Respawn()
 		{
 			base.Respawn();
 
-			animationPlayer.Play("spawn");
-			animationPlayer.Seek(0.0, true);
+			// Snap out of the defeat animation
+			defeatTransition.XfadeTime = 0;
+			animationTree.Set(DEFEAT_TRANSITION, DISABLED_CONSTANT);
 
+			// Reset to passive state
+			currentState = State.Passive;
+			animationTree.Set(STATE_TRANSITION, PASSIVE_STATE);
+
+			// Reset variables
 			stateTimer = 0;
-			currentState = States.Passive;
-
 			rotationVelocity = 0;
 
+			// Remove seeds
 			seedIndex = 0;
 			for (int i = 0; i < MAX_SEED_COUNT; i++)
 			{
@@ -75,17 +116,18 @@ namespace Project.Gameplay
 			}
 		}
 
+
 		protected override void UpdateInteraction()
 		{
-			if (isOpen || Character.ActionState != CharacterController.ActionStates.JumpDash)
+			if (!IsOpen && Character.ActionState == CharacterController.ActionStates.JumpDash)
 			{
-				base.UpdateInteraction();
+				Character.Lockon.StartBounce();
 				return;
 			}
 
-			Character.Lockon.StartBounce(IsDefeated);
-			Character.Camera.LockonTarget = this;
+			base.UpdateInteraction();
 		}
+
 
 		public override void TakePlayerDamage()
 		{
@@ -93,118 +135,139 @@ namespace Project.Gameplay
 
 			if (IsDefeated)
 			{
-				animationPlayer.Play("defeat");
+				StartDefeatState();
 				return;
 			}
 
-			animationPlayer.Play("hit");
-			if (currentState == States.Attack)
-				currentState = States.PostAttack;
+			StartStaggerState();
 		}
+
 
 		protected override void UpdateEnemy()
 		{
 			if (IsDefeated) return;
 
-			if (IsInRange || currentState != States.Passive)
+			if (IsInRange || currentState != State.Passive)
 			{
 				UpdateRotation();
 				UpdateState();
 			}
 		}
 
+
 		private void UpdateRotation()
 		{
-			if (isOpen)
-			{
-				//Rotate towards the player
-				TrackPlayer();
+			if (!IsOpen) return;
 
-				//Update movement
-			}
+			// TODO Update movement
 
-			root.Rotation = new Vector3(root.Rotation.X, currentRotation, root.Rotation.Z);
+
+			// Rotate towards the player
+			TrackPlayer();
+			root.Rotation = new Vector3(root.Rotation.X, currentRotation, root.Rotation.Z); // Apply rotation
 		}
 
-		private float stateTimer;
-		private States currentState;
-		private enum States
-		{
-			Passive,
-			PreAttack,
-			Attack,
-			PostAttack
-		}
+
 		private void UpdateState()
 		{
-			if (isInHitstun || currentState == States.Attack) return;
+			if (currentState == State.Attack)
+				return; // Let the attack finish
+
+			if (IsStaggered)
+				return; // Let stagger finish
+
 
 			stateTimer += PhysicsManager.physicsDelta;
 			switch (currentState)
 			{
-				case States.Passive:
+				case State.Passive:
 					if (stateTimer >= passiveLength)
-					{
-						animationPlayer.Play("show");
-
-						stateTimer = 0;
-						currentState = States.PreAttack;
-					}
+						StartAggressiveState();
 					break;
-				case States.PreAttack:
+				case State.PreAttack:
 					if (stateTimer >= preAttackLength)
 					{
 						if (IsInRange)
-						{
-							animationPlayer.Play("attack");
-							currentState = States.Attack;
-						}
-						else //Player is out of range
-						{
-							animationPlayer.Play("hide");
-							currentState = States.Passive;
-						}
-
-						stateTimer = 0;
+							StartAttackState();
+						else // Player has left range; return to passive state
+							StartPassiveState();
 					}
 					break;
-				case States.PostAttack:
+				case State.PostAttack:
 					if (stateTimer >= postAttackLength)
-					{
-						animationPlayer.Play("hide");
-						currentState = States.Passive;
-						stateTimer = 0;
-					}
+						StartPassiveState();
 					break;
 			}
 		}
 
-		public void IncrementAttack()
+
+		private void StartPassiveState()
 		{
-			if (seedIndex >= MAX_SEED_COUNT)
-			{
-				seedIndex = 0;
-				stateTimer = 0;
-				currentState = States.PostAttack;
-			}
-			else
-			{
-				animationPlayer.Play("attack");
-				animationPlayer.Seek(0.0, true);
-			}
+			stateTimer = 0;
+			currentState = State.Passive;
+			animationTree.Set(STATE_TRANSITION, HIDE_STATE);
 		}
 
-		public void FireAttack()
+
+		private void StartAggressiveState()
 		{
-			if (!seedPool[seedIndex].IsInsideTree()) //Add to tree
+			stateTimer = 0;
+			currentState = State.PreAttack;
+			animationTree.Set(STATE_TRANSITION, SHOW_STATE);
+		}
+
+
+		private void StartAttackState()
+		{
+			stateTimer = 0;
+			currentState = State.Attack;
+			animationTree.Set(ATTACK_TRIGGER, (int)AnimationNodeOneShot.OneShotRequest.Fire);
+		}
+
+
+		private void IncrementAttack()
+		{
+			if (seedIndex < MAX_SEED_COUNT) // Fire another seed
+			{
+				StartAttackState();
+				return;
+			}
+
+			StopAttackState();
+		}
+
+
+		/// <summary> Launches a seed at the player. </summary>
+		private void FireAttack()
+		{
+			if (!seedPool[seedIndex].IsInsideTree()) // Add seeds to the scene tree
 				GetTree().Root.AddChild(seedPool[seedIndex]);
 
-			Vector3 shotDirection = hurtbox.GlobalPosition - Character.CenterPosition;
-			shotDirection -= Vector3.Up * .4f; //Aim slightly higher so seeds actually hit the player instead of the ground
-			seedPool[seedIndex].LookAtFromPosition(hurtbox.GlobalPosition, hurtbox.GlobalPosition + shotDirection, Vector3.Up);
+			Vector3 targetOffset = hurtbox.GlobalPosition - Character.CenterPosition;
+			targetOffset -= Vector3.Up * .4f; // Aim slightly higher so seeds avoid hitting the ground
+			seedPool[seedIndex].LookAtFromPosition(hurtbox.GlobalPosition, hurtbox.GlobalPosition + targetOffset, Vector3.Up);
 			seedPool[seedIndex].Spawn();
 
-			seedIndex++; //Increment counter
+			seedIndex++; // Increment counter
 		}
+
+
+		private void StopAttackState()
+		{
+			seedIndex = 0;
+			stateTimer = 0;
+			currentState = State.PostAttack;
+		}
+
+
+		private void StartStaggerState()
+		{
+			animationTree.Set(STAGGER_TRIGGER, (int)AnimationNodeOneShot.OneShotRequest.Fire);
+			if (currentState == State.Attack)
+				StopAttackState();
+		}
+
+
+		private void StartDefeatState() => animationTree.Set(DEFEAT_TRANSITION, ENABLED_CONSTANT);
 	}
 }
