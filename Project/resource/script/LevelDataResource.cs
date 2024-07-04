a863e8d28b10192a276e9e2ae97d9bf6ce81ec1d
Kuma-Boo <kumapauze@gmail.com>
2024-07-04 17:16:57 -0400

    Level Tweaks

--- Project/resource/script/LevelDataResource.cs
+++ Project/resource/script/LevelDataResource.cs
@@ -53,7 +53,7 @@ public partial class LevelDataResource : Resource
 		properties.Add(ExtensionMethods.CreateProperty("Ranking/Bronze Time", Variant.Type.Int));
 
 		if (!SkipScore)
-			properties.Add(ExtensionMethods.CreateProperty("Ranking/Score Requirement", Variant.Type.Int, PropertyHint.Range, "0,99999999,100"));
+			properties.Add(ExtensionMethods.CreateProperty("Ranking/Score Requirement", Variant.Type.Int, PropertyHint.Range, "0,999999,100"));
 
 		properties.Add(ExtensionMethods.CreateProperty("Completion/Delay", Variant.Type.Float, PropertyHint.Range, "0,5,.1"));
 		properties.Add(ExtensionMethods.CreateProperty("Completion/Lockout", Variant.Type.Object)); //, PropertyHint.ResourceType, "LockoutResource"));
