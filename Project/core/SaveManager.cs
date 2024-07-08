8b8e4e2dd8ae94d9311a5abae5f6dc9c47f68871
Kuma <kumapauze@gmail.com>
2024-07-08 11:20:30 -0400

    Exp fixes (#171)
    * Fix score respawn
    
    * Fix reverting data when respawning
    
    * Add level cap
    
    * Update max exp bar
    
    * Update level calculation
    
    * Add fallback for old save data
    
    * Fix level detection

--- Project/core/SaveManager.cs
+++ Project/core/SaveManager.cs
@@ -561,7 +561,7 @@ public partial class SaveManager : Node
 		public Array<WorldEnum> worldRingsCollected = [];
 		/// <summary> List of worlds unlocked. </summary>
 		public Array<WorldEnum> worldsUnlocked = [];
-		/// <summary> List of worlds unlocked. </summary>
+		/// <summary> List of stages unlocked. </summary>
 		public Array<string> stagesUnlocked = [];
 
 		/// <summary> Player level, from 1 -> 99 </summary>
@@ -806,19 +806,26 @@ public partial class SaveManager : Node
 			// WorldEnum data
 			if (dictionary.TryGetValue(nameof(lastPlayedWorld), out Variant var))
 				lastPlayedWorld = (WorldEnum)(int)var;
-			if (dictionary.TryGetValue(nameof(worldsUnlocked), out var))
+			if (dictionary.TryGetValue(nameof(worldsUnlocked), out var) && var.VariantType == Variant.Type.Array)
 			{
 				Array<int> worlds = (Array<int>)var;
 				for (int i = 0; i < worlds.Count; i++)
 					worldsUnlocked.Add((WorldEnum)worlds[i]);
 			}
-			if (dictionary.TryGetValue(nameof(worldRingsCollected), out var))
+			else
+			{
+				// Update save data from old format (unlock everything to prevent softlocks)
+				for (int i = 0; i < (int)WorldEnum.Max; i++)
+					worldsUnlocked.Add((WorldEnum)i);
+			}
+
+			if (dictionary.TryGetValue(nameof(worldRingsCollected), out var) && var.VariantType == Variant.Type.Array)
 			{
 				Array<int> worlds = (Array<int>)var;
 				for (int i = 0; i < worlds.Count; i++)
 					worldRingsCollected.Add((WorldEnum)worlds[i]);
 			}
-			if (dictionary.TryGetValue(nameof(worldsUnlocked), out var))
+			if (dictionary.TryGetValue(nameof(stagesUnlocked), out var) && var.VariantType == Variant.Type.Array)
 				stagesUnlocked = (Array<string>)var;
 
 			if (dictionary.TryGetValue(nameof(levelData), out var))
--- Project/object/trigger/modules/CheckpointTrigger.cs
+++ Project/object/trigger/modules/CheckpointTrigger.cs
@@ -10,20 +10,16 @@ namespace Project.Gameplay.Triggers
 		public Path3D PlayerPath { get; private set; }
 		public Path3D CameraPath { get; private set; }
 		public CameraSettingsResource CameraSettings;
-		private int savedObjectiveCount;
+
+		private StageSettings Stage => StageSettings.instance;
 
 		public override void Activate()
 		{
-			StageSettings.instance.SetCheckpoint(this);
-			StageSettings.instance.ResetObjective(savedObjectiveCount);
-		}
+			Stage.SetCheckpoint(this);
 
-		public void UpdateCheckpointData()
-		{
 			PlayerPath = CharacterController.instance.PathFollower.ActivePath; // Store current player path
 			CameraPath = CharacterController.instance.Camera.PathFollower.ActivePath; // Store current camera path
 			CameraSettings = CharacterController.instance.Camera.ActiveSettings;
-			savedObjectiveCount = StageSettings.instance.CurrentObjectiveCount;
 		}
 	}
 }
