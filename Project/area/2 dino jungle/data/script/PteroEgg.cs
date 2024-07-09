726795ddf0f6f91e7ca320d858943caad7443bb1
Kuma-Boo <kumapauze@gmail.com>
2024-07-08 20:14:04 -0400

    Started working on dino egg act

--- Project/area/2 dino jungle/data/script/PteroEgg.cs
+++ Project/area/2 dino jungle/data/script/PteroEgg.cs
@@ -1,109 +1,110 @@
 using Godot;
 using Project.Core;
 
-namespace Project.Gameplay.Objects
+namespace Project.Gameplay.Objects;
+
+/// <summary> For that one act in Dinosaur Jungle. Follows the player until damage is taken. </summary>
+public partial class PteroEgg : Area3D
 {
-	/// <summary>
-	/// For that one act in Dinosaur Jungle. Follows the player until they take damage.
-	/// </summary>
-	public partial class PteroEgg : Area3D
+	/// <summary> Emitted when the egg is back in the nest. </summary>
+	[Signal]
+	public delegate void ReturnedEventHandler();
+
+	[Export(PropertyHint.NodePathValidTypes, "Node3D")]
+	private NodePath root;
+	private Node3D Root { get; set; }
+	[Export(PropertyHint.NodePathValidTypes, "AnimationPlayer")]
+	private NodePath animator;
+	private AnimationPlayer Animator { get; set; }
+
+	/// <summary> How close is this egg to the player? </summary>
+	public int EggIndex { get; set; }
+	/// <summary> Egg is sleeping, and can no longer be interacted with. </summary>
+	private bool isSleeping;
+	/// <summary> Has the egg been returned to the nest successfully? </summary>
+	private bool isReturnedToNest;
+
+	private float returnTravelRatio;
+	private SpawnData spawnData;
+	private LaunchSettings returnArc; // The path to follow when returning to the nest
+	private CharacterController Character => CharacterController.instance;
+	private readonly float FollowDistance = 1f;
+
+	public override void _Ready()
 	{
-		[Signal]
-		public delegate void ReturnedEventHandler(); //Emitted when the egg is back in the nest
+		spawnData = new SpawnData(GetParent(), Transform);
+		StageSettings.instance.ConnectRespawnSignal(this);
+	}
 
-		[Export]
-		private Node3D root;
-		[Export]
-		private AnimationPlayer animator;
+	public override void _PhysicsProcess(double _)
+	{
+		if (isSleeping) return;
 
-		public int EggIndex { get; set; } //Is this the first (closest) egg, or the second (farther) egg? or, is this egg not being carried? (index 0)
-		private bool isSleeping; //Egg is sleeping, and can no longer be interacted with
-		private bool isReturnedToNest; //Has the egg been returned to the nest?
-
-		private float returnTravelRatio;
-		private SpawnData spawnData;
-		private LaunchSettings returnArc; //The path to follow when returning to the nest
-		private CharacterController Character => CharacterController.instance;
-		private readonly float FOLLOW_DISTANCE = 1f;
-
-		public override void _Ready()
+		if (EggIndex != 0)
 		{
-			spawnData = new SpawnData(GetParent(), Transform);
-
-			StageSettings.instance.ConnectRespawnSignal(this);
+			// Update position to trail player
+			GlobalPosition = Character.GlobalPosition + (Character.PathFollower.Back() * FollowDistance);
 		}
-
-		public override void _PhysicsProcess(double _)
+		else if (isReturnedToNest)
 		{
-			if (isSleeping) return;
+			if (Mathf.IsZeroApprox(returnTravelRatio))
+				Animator.Play("returned", .2f);
 
-			if (EggIndex != 0)
+			returnTravelRatio = Mathf.MoveToward(returnTravelRatio, 1f, PhysicsManager.physicsDelta);
+			GlobalPosition = returnArc.InterpolatePositionRatio(returnTravelRatio);
+
+			if (Mathf.IsEqualApprox(returnTravelRatio, 1))
 			{
-				//Update position to trail player
-				GlobalPosition = Character.GlobalPosition + Character.PathFollower.Back() * FOLLOW_DISTANCE;
-			}
-			else if (isReturnedToNest)
-			{
-				if (Mathf.IsZeroApprox(returnTravelRatio))
-					animator.Play("returned", .2f);
-
-				returnTravelRatio = Mathf.MoveToward(returnTravelRatio, 1f, PhysicsManager.physicsDelta);
-				GlobalPosition = returnArc.InterpolatePositionRatio(returnTravelRatio);
-
-				if (Mathf.IsEqualApprox(returnTravelRatio, 1))
-				{
-					isSleeping = true;
-					EmitSignal(SignalName.Returned);
-				}
+				isSleeping = true;
+				EmitSignal(SignalName.Returned);
 			}
 		}
+	}
 
-		public void Frighten() //Called when the player takes damage, dies, or a third egg is picked up.
-		{
-			EggIndex = 0;
-			animator.Play("frighten");
-		}
+	public void Frighten() // Called when the player takes damage, dies, or a third egg is picked up.
+	{
+		EggIndex = 0;
+		Animator.Play("frighten");
+	}
 
-		private void Respawn()
-		{
-			if (isReturnedToNest) return; //Don't respawn if we're already at the nest. Don't force the player to redo stuff they already did.
+	private void Respawn()
+	{
+		if (isReturnedToNest) return; // Don't respawn if we're already at the nest. Don't force the player to redo stuff they already did.
 
-			if (EggIndex != 0)
-			{
-				PteroEggManager.DropEgg(this);
-			}
-
-			spawnData.Respawn(this);
-			animator.Play("idle");
-		}
-
-		public void SetType(Node3D model) //Adds the egg model as a child
-		{
-			root.CallDeferred("add_child", model);
-			model.SetDeferred("global_transform", GlobalTransform);
-		}
-
-		public void OnEntered(Area3D a)
-		{
-			if (!a.IsInGroup("player")) return;
-
-			Character.Connect(CharacterController.SignalName.Knockback, new Callable(this, MethodName.Frighten), (uint)ConnectFlags.OneShot);
-			PteroEggManager.PickUpEgg(this);
-		}
-
-		public void ReturnToNest(PteroNest nest)
-		{
+		if (EggIndex != 0)
 			PteroEggManager.DropEgg(this);
-			isReturnedToNest = true;
 
-			Vector3 delta = GlobalPosition - nest.GlobalPosition;
+		spawnData.Respawn(this);
+		Animator.Play("idle");
+	}
 
-			GetParent().CallDeferred("remove_child", this);
-			nest.CallDeferred("add_child", this);
-			SetDeferred("global_position", nest.GlobalPosition + delta);
+	public void SetType(Node3D model) // Adds the egg model as a child
+	{
+		Root.CallDeferred("add_child", model);
+		model.SetDeferred("global_transform", GlobalTransform);
+		model.SetDeferred("global_transform", GlobalTransform);
+	}
 
-			returnTravelRatio = 0f;
-			returnArc = LaunchSettings.Create(GlobalPosition, nest.GlobalPosition + Vector3.Up * 0.6f, 4f, true);
-		}
+	public void OnEntered(Area3D a)
+	{
+		if (!a.IsInGroup("player detection")) return;
+
+		Character.Connect(CharacterController.SignalName.Knockback, new Callable(this, MethodName.Frighten), (uint)ConnectFlags.OneShot);
+		PteroEggManager.PickUpEgg(this);
+	}
+
+	public void ReturnToNest(PteroNest nest)
+	{
+		PteroEggManager.DropEgg(this);
+		isReturnedToNest = true;
+
+		Vector3 delta = GlobalPosition - nest.GlobalPosition;
+
+		GetParent().CallDeferred("remove_child", this);
+		nest.CallDeferred("add_child", this);
+		SetDeferred("global_position", nest.GlobalPosition + delta);
+
+		returnTravelRatio = 0f;
+		returnArc = LaunchSettings.Create(GlobalPosition, nest.GlobalPosition + (Vector3.Up * 0.6f), 4f, true);
 	}
 }
--- Project/area/2 dino jungle/data/script/PteroEggManager.cs
+++ Project/area/2 dino jungle/data/script/PteroEggManager.cs
@@ -2,103 +2,96 @@ using Godot;
 using Godot.Collections;
 using Project.Core;
 
-namespace Project.Gameplay.Objects
+namespace Project.Gameplay.Objects;
+
+/// <summary> Responsible for handling the types of eggs that spawn. </summary>
+public partial class PteroEggManager : Node3D
 {
-	/// <summary>
-	/// Responsible for handling the types of eggs that spawn.
-	/// </summary>
-	public partial class PteroEggManager : Node3D
+	// In the original game, you could only hold one egg at a time; You can now hold as many as you run across 
+	private static PteroEgg eggOne; // First held egg
+	private static PteroEgg eggTwo; // Second held egg
+	public static bool IsEggHeld(PteroEgg egg) => eggOne == egg || eggTwo == egg;
+	public static void DropEgg(PteroEgg egg)
 	{
-		//In the original game, you could only hold one egg at a time.
-		//You can now hold 2. 
-		private static PteroEgg eggOne; //First held egg
-		private static PteroEgg eggTwo; //Second held egg
-		public static bool IsEggHeld(PteroEgg egg) => eggOne == egg || eggTwo == egg;
-		public static void DropEgg(PteroEgg egg)
+		if (eggOne == egg)
 		{
-			if (eggOne == egg)
-			{
-				eggOne.Frighten();
-				eggOne = eggTwo;
-			}
-
-			eggTwo = null;
-			egg.EggIndex = 0;
-		}
-		public static void PickUpEgg(PteroEgg egg)
-		{
-			if (eggTwo != null) //Too many eggs!
-				DropEgg(eggOne);
-
-			if (eggOne == null)
-			{
-				eggOne = egg;
-				egg.EggIndex = 1;
-			}
-			else
-			{
-				eggTwo = egg;
-				egg.EggIndex = 2;
-			}
+			eggOne.Frighten();
+			eggOne = eggTwo;
 		}
 
-		//Models
-		[Export]
-		public PackedScene[] eggs;
-		[Export]
-		public PackedScene[] signs;
-
-		private Array<PteroEgg> _eggs = new Array<PteroEgg>();
-		private Array<PteroNest> _nests = new Array<PteroNest>();
-
-		public override void _Ready()
+		eggTwo = null;
+		egg.EggIndex = 0;
+	}
+	public static void PickUpEgg(PteroEgg egg)
+	{
+		if (eggOne == null)
 		{
-			for (int i = 0; i < GetChildCount(); i++)
-			{
-				Node node = GetChild(i);
-				if (node is PteroEgg)
-					_eggs.Add(node as PteroEgg);
-				else if (node is PteroNest)
-					_nests.Add(node as PteroNest);
-			}
+			eggOne = egg;
+			egg.EggIndex = 1;
+		}
+		else
+		{
+			eggTwo = egg;
+			egg.EggIndex = 2;
+		}
+	}
 
-			//Number of eggs and nests MUST BE THE SAME!!!
-			for (int i = 0; i < _eggs.Count; i++)
-			{
-				int type = GenerateEggType();
-				_eggs[i].SetType(eggs[type].Instantiate<Node3D>());
-				_nests[GenerateEggPair(i)].SetType(signs[type].Instantiate<Node3D>());
-			}
+	// Models
+	[Export]
+	public PackedScene[] eggModels;
+	[Export]
+	public PackedScene[] signModels;
+
+	private Array<PteroEgg> eggs = [];
+	private Array<PteroNest> nests = [];
+
+	public override void _Ready()
+	{
+		for (int i = 0; i < GetChildCount(); i++)
+		{
+			Node node = GetChild(i);
+			if (node is PteroEgg)
+				eggs.Add(node as PteroEgg);
+			else if (node is PteroNest)
+				nests.Add(node as PteroNest);
 		}
 
-		public override void _ExitTree() //Clear memory
+		// Number of eggs and nests MUST BE THE SAME!!!
+		for (int i = 0; i < eggs.Count; i++)
 		{
-			eggOne = eggTwo = null;
+			int type = GenerateEggType();
+			eggs[i].SetType(eggModels[type].Instantiate<Node3D>());
+			nests[GenerateEggPair(i)].SetType(signModels[type].Instantiate<Node3D>());
+		}
+	}
+
+	public override void _ExitTree() // Clear memory
+	{
+		eggOne = eggTwo = null;
+	}
+
+	private Array<int> _excludedTypes = new Array<int>(); // Eggs that have already been generated
+	private int GenerateEggType()
+	{
+		int type = Runtime.randomNumberGenerator.RandiRange(0, 8);
+		while (_excludedTypes.Contains(type)) // Regenerate type if it's already been taken
+		{
+			type = Runtime.randomNumberGenerator.RandiRange(0, 8);
 		}
 
-		private Array<int> _excludedTypes = new Array<int>(); //Eggs that have already been generated
-		private int GenerateEggType()
-		{
-			int type = Runtime.randomNumberGenerator.RandiRange(0, 8);
-			while (_excludedTypes.Contains(type)) //Regenerate type if it's already been taken
-			{
-				type = Runtime.randomNumberGenerator.RandiRange(0, 8);
-			}
+		_excludedTypes.Add(type);
+		return type;
+	}
 
-			_excludedTypes.Add(type);
-			return type;
+	private int GenerateEggPair(int eggIndex) // Determines which egg belongs to which nest
+	{
+		int index = Runtime.randomNumberGenerator.RandiRange(0, eggs.Count - 1);
+		while (nests[index].AssignedEgg != null) // Nest is already taken
+		{
+			index = Runtime.randomNumberGenerator.RandiRange(0, eggs.Count - 1);
 		}
 
-		private int GenerateEggPair(int eggIndex) //Determines which egg belongs to which nest
-		{
-			int index = Runtime.randomNumberGenerator.RandiRange(0, _eggs.Count - 1);
-			while (_nests[index].AssignedEgg != null) //Nest is already taken
-			{
-				index = Runtime.randomNumberGenerator.RandiRange(0, _eggs.Count - 1);
-			}
-
-			_nests[index].AssignedEgg = _eggs[eggIndex];
-			return index;
-		}
+		nests[index].AssignedEgg = eggs[eggIndex];
+		return index;
 	}
 }
