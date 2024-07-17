7a663c458d12a318e96151634d52e36453cdcdd9
Kuma-Boo <kumapauze@gmail.com>
2024-07-17 15:08:13 -0400

    Fix save unlocking

--- Project/interface/menu/save select/SaveSelect.cs
+++ Project/interface/menu/save select/SaveSelect.cs
@@ -116,9 +116,6 @@ public partial class SaveSelect : Menu
 		SaveManager.ActiveSaveSlotIndex = _saveOptions[ActiveSaveOptionIndex].SaveIndex;
 		SaveManager.ActiveSkillRing.LoadFromActiveData();
 
-		if (DebugManager.Instance.UseDemoSave || OS.IsDebugBuild()) // Unlock all worlds in the demo
-			SaveManager.ActiveGameData.UnlockAllWorlds();
-
 		if (SaveManager.ActiveGameData.IsNewFile())
 		{
 			SaveManager.ResetSaveData(SaveManager.ActiveSaveSlotIndex, false);
@@ -135,6 +132,9 @@ public partial class SaveSelect : Menu
 				});
 			}
 		}
+
+		if (DebugManager.Instance.UseDemoSave || OS.IsDebugBuild()) // Unlock all worlds in the demo
+			SaveManager.ActiveGameData.UnlockAllWorlds();
 
 		menuMemory[MemoryKeys.WorldSelect] = (int)SaveManager.ActiveGameData.lastPlayedWorld; // Set the world selection to the last played world
 		_submenus[0].ShowMenu();
