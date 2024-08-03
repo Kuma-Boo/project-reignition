262f1c3d24b6c65140574a04f4ce0a7af18cae31
Kuma-Boo <kumapauze@gmail.com>
2024-08-03 14:22:57 -0400

    Fix falling transitions

--- Project/object/player/resource/script/CharacterController.cs
+++ Project/object/player/resource/script/CharacterController.cs
@@ -309,6 +309,7 @@ namespace Project.Gameplay
 		}
 
 		private float jumpBufferTimer;
+		public void ResetJumpBuffer() => jumpBufferTimer = 0;
 		private float actionBufferTimer;
 		private const float ACTION_BUFFER_LENGTH = .2f; // How long to allow actions to be buffered
 		private const float JUMP_BUFFER_LENGTH = .1f; // How long to allow jumps to be buffered
@@ -1795,6 +1796,8 @@ namespace Project.Gameplay
 				UpDirection = Vector3.Up;
 				Effect.PlayActionSFX(Effect.JumpSfx);
 			}
+
+			UpdateLauncher();
 		}
 
 		private void UpdateLauncher()
@@ -1804,6 +1807,7 @@ namespace Project.Gameplay
 			if (activeLauncher?.IsCharacterCentered == false)
 			{
 				GlobalPosition = activeLauncher.RecenterCharacter();
+				VerticalSpeed = LaunchSettings.InitialVelocity.Y;
 			}
 			else
 			{
