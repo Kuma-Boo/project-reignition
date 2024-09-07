1066d294ed507b8480eea5701542b19d58114fee
Kuma-Boo <kumapauze@gmail.com>
2024-09-06 20:50:45 -0400

    Add framerate limiter

--- Project/core/SaveManager.cs
+++ Project/core/SaveManager.cs
@@ -98,6 +98,14 @@ public partial class SaveManager : Node
 		new(3840, 2160), // 4K
 	];
 
+	public static readonly int[] FrameRates =
+	[
+		0,
+		30,
+		60,
+		120,
+	];
+
 	#endregion
 
 	public partial class ConfigData : GodotObject
@@ -107,6 +115,7 @@ public partial class SaveManager : Node
 		public int windowSize = 3; // Defaults to one lower than 1080p
 		public bool useFullscreen = true;
 		public bool useExclusiveFullscreen;
+		public int framerate = 2;
 		public bool useVsync;
 		public int renderScale = 100;
 		public RenderingServer.ViewportScaling3DMode resizeMode = RenderingServer.ViewportScaling3DMode.Bilinear;
@@ -149,6 +158,7 @@ public partial class SaveManager : Node
 				{ nameof(windowSize), windowSize },
 				{ nameof(useFullscreen), useFullscreen },
 				{ nameof(useExclusiveFullscreen), useExclusiveFullscreen },
+				{ nameof(framerate), framerate },
 				{ nameof(useVsync), useVsync },
 
 				{ nameof(renderScale), renderScale },
@@ -195,6 +205,8 @@ public partial class SaveManager : Node
 				useExclusiveFullscreen = (bool)var;
 			if (dictionary.TryGetValue(nameof(windowSize), out var))
 				windowSize = (int)var;
+			if (dictionary.TryGetValue(nameof(framerate), out var))
+				framerate = (int)var;
 			if (dictionary.TryGetValue(nameof(useVsync), out var))
 				useVsync = (bool)var;
 
@@ -303,6 +315,7 @@ public partial class SaveManager : Node
 		if (!Config.useFullscreen)
 			DisplayServer.WindowSetSize(WindowSizes[Config.windowSize]);
 
+		Engine.MaxFps = FrameRates[Config.framerate];
 		DisplayServer.VSyncMode targetVSyncMode =
 			Config.useVsync ? DisplayServer.VSyncMode.Enabled : DisplayServer.VSyncMode.Disabled;
 		if (DisplayServer.WindowGetVsyncMode() != targetVSyncMode)
