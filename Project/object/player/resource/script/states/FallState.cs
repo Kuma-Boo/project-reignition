656bc909730396586fc290b5206529ed7697dbf5
Kuma-Boo <kumapauze@gmail.com>
2024-09-11 22:05:16 -0400

    Fix Fall State turning

--- Project/object/player/resource/script/states/FallState.cs
+++ Project/object/player/resource/script/states/FallState.cs
@@ -16,7 +16,8 @@ public partial class FallState : PlayerState
 
 	public override PlayerState ProcessPhysics()
 	{
-		UpdateMoveSpeed();
+		ProcessMoveSpeed();
+		ProcessTurning();
 		Player.VerticalSpeed = Mathf.MoveToward(Player.VerticalSpeed, Runtime.MaxGravity, Runtime.Gravity * PhysicsManager.physicsDelta);
 		Player.ApplyMovement();
 		Player.IsMovingBackward = Player.Controller.IsHoldingDirection(Player.MovementAngle, Player.PathFollower.BackAngle);
@@ -43,47 +44,5 @@ public partial class FallState : PlayerState
 		}
 
 		return null;
-	}
-
-	private void UpdateMoveSpeed()
-	{
-		float inputStrength = Player.Controller.GetInputStrength();
-		if (Mathf.IsZeroApprox(inputStrength))
-		{
-			Player.MoveSpeed = Player.Stats.AirSettings.UpdateInterpolate(Player.MoveSpeed, 0);
-			return;
-		}
-
-		float targetMovementAngle = Player.Controller.GetTargetMovementAngle();
-		float inputDot = ExtensionMethods.DotAngle(Player.MovementAngle, targetMovementAngle);
-		if ((inputDot < -.75f && !Mathf.IsZeroApprox(Player.MoveSpeed)) || Input.IsActionPressed("button_brake")) // Turning around
-		{
-			Player.MoveSpeed = Player.Stats.AirSettings.UpdateInterpolate(Player.MoveSpeed, -inputStrength);
-			return;
-		}
-
-		UpdateTurning(targetMovementAngle);
-		Player.MoveSpeed = Player.Stats.AirSettings.UpdateInterpolate(Player.MoveSpeed, inputStrength);
-	}
-
-	private void UpdateTurning(float targetMovementAngle)
-	{
-		if (Mathf.IsZeroApprox(Player.MoveSpeed))
-		{
-			Player.MovementAngle = targetMovementAngle;
-			return;
-		}
-		float speedRatio = Player.Stats.GroundSettings.GetSpeedRatioClamped(Player.MoveSpeed);
-
-		targetMovementAngle += Player.PathTurnInfluence;
-		targetMovementAngle = Player.Controller.ImproveAnalogPrecision(targetMovementAngle, Player.PathFollower.ForwardAngle);
-
-		float inputDeltaAngle = ExtensionMethods.SignedDeltaAngleRad(targetMovementAngle, Player.PathFollower.ForwardAngle);
-		float movementDeltaAngle = ExtensionMethods.SignedDeltaAngleRad(Player.MovementAngle, Player.PathFollower.ForwardAngle);
-		bool isRecentering = Player.Controller.IsRecentering(movementDeltaAngle, inputDeltaAngle);
-		float maxTurnAmount = isRecentering ? Player.Stats.RecenterTurnAmount : Player.Stats.MaxTurnAmount;
-
-		float turnSmoothing = Mathf.Lerp(Player.Stats.MinTurnAmount, maxTurnAmount, speedRatio);
-		Player.MovementAngle = ExtensionMethods.SmoothDampAngle(Player.MovementAngle + Player.PathTurnInfluence, targetMovementAngle, ref turningVelocity, turnSmoothing);
 	}
 }
