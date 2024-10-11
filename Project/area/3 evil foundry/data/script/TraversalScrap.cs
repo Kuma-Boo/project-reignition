6541e74f6d6d9de492a1468aaf5fa3898474d789
Kuma-Boo <kumapauze@gmail.com>
2024-10-10 22:25:24 -0400

    Add Traversal Scraps

--- Project/area/3 evil foundry/data/script/TraversalScrap.cs
+++ Project/area/3 evil foundry/data/script/TraversalScrap.cs
@@ -1,16 +1,32 @@
 using Godot;
-using System;
 
 namespace Project.Gameplay;
 
 /// <summary> Controls the behavior of the flying scrap used in the Ifrit Golem boss fight. </summary>
 public partial class TraversalScrap : Area3D
 {
+	[Export] private NodePath animator;
+	private AnimationPlayer _animator;
 	private bool isFalling;
 	private bool isInteractingWithPlayer;
+	private SpawnData spawnData;
 	private PlayerController Player => StageSettings.Player;
 
-	public override void _Process(double _)
+	public override void _Ready()
+	{
+		_animator = GetNode<AnimationPlayer>(animator);
+		spawnData = new SpawnData(GetParent(), Transform);
+		_animator.Play("init");
+	}
+
+	private void Respawn()
+	{
+		isFalling = false;
+		_animator.Play("show");
+		spawnData.Respawn(this);
+	}
+
+	public override void _PhysicsProcess(double _)
 	{
 		if (!isInteractingWithPlayer)
 			return;
@@ -19,6 +35,23 @@ public partial class TraversalScrap : Area3D
 			return;
 
 		Player.StartBounce();
+		isInteractingWithPlayer = false;
+		_animator.Play("fall");
+	}
 
+	public void OnEntered(Area3D a)
+	{
+		if (!a.IsInGroup("player detection"))
+			return;
+
+		isInteractingWithPlayer = true;
+	}
+
+	public void OnExited(Area3D a)
+	{
+		if (!a.IsInGroup("player detection"))
+			return;
+
+		isInteractingWithPlayer = false;
 	}
 }
