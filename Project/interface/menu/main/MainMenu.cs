fd9217a50b1d23ffae2f3ed97d58e606e0fed39d
Kuma-Boo <kumapauze@gmail.com>
2024-11-22 14:25:56 -0500

    Fix main menu description

--- Project/interface/menu/main/MainMenu.cs
+++ Project/interface/menu/main/MainMenu.cs
@@ -2,160 +2,159 @@ using Godot;
 using Godot.Collections;
 using Project.Core;
 
-namespace Project.Interface.Menus
+namespace Project.Interface.Menus;
+
+public partial class MainMenu : Menu
 {
-	public partial class MainMenu : Menu
+	[Export] private Description description;
+	[Export] private Node2D optionParent;
+	[Export] private Node2D cursor;
+	private Vector2 cursorVelocity;
+	private readonly Array<Node2D> options = [];
+	private const float CursorSmoothing = .08f;
+
+	private int currentSelection;
+
+	public override void ShowMenu()
 	{
-		[Export]
-		private Node2D optionParent;
-		private readonly Array<Node2D> options = new();
-		[Export]
-		private Node2D cursor;
-		private Vector2 cursorVelocity;
-		private const float CURSOR_SMOOTHING = .08f;
+		base.ShowMenu();
+		cursorVelocity = Vector2.Zero;
+		cursor.Position = options[currentSelection].Position;
+		menuMemory[MemoryKeys.ActiveMenu] = (int)MemoryKeys.MainMenu;
 
-		private int currentSelection;
+		SetUp();
+	}
 
-		public override void ShowMenu()
+	protected override void SetUp()
+	{
+		for (int i = 0; i < optionParent.GetChildCount(); i++)
+			options.Add(optionParent.GetChild<Node2D>(i));
+
+		currentSelection = menuMemory[MemoryKeys.MainMenu];
+		HorizontalSelection = currentSelection % 2;
+		VerticalSelection = currentSelection / 2;
+
+		isProcessing = menuMemory[MemoryKeys.ActiveMenu] == (int)MemoryKeys.MainMenu;
+	}
+
+	protected override void ProcessMenu()
+	{
+		if (!isQuitMenuActive && Input.IsActionJustPressed("button_pause"))
 		{
-			base.ShowMenu();
-			cursorVelocity = Vector2.Zero;
-			cursor.Position = options[currentSelection].Position;
-			menuMemory[MemoryKeys.ActiveMenu] = (int)MemoryKeys.MainMenu;
-
-			SetUp();
+			quitAnimator.Play("show");
+			isQuitMenuActive = true;
+			isQuitSelected = false;
+			return;
 		}
 
-		protected override void SetUp()
+		base.ProcessMenu();
+		cursor.Position = cursor.Position.SmoothDamp(options[currentSelection].Position, ref cursorVelocity, CursorSmoothing);
+	}
+
+	[Export]
+	private AnimationPlayer quitAnimator;
+	private bool isQuitMenuActive;
+	private bool isQuitSelected;
+	private void CancelQuitMenu()
+	{
+		if (isQuitSelected)
 		{
-			for (int i = 0; i < optionParent.GetChildCount(); i++)
-				options.Add(optionParent.GetChild<Node2D>(i));
-
-			currentSelection = menuMemory[MemoryKeys.MainMenu];
-			HorizontalSelection = currentSelection % 2;
-			VerticalSelection = currentSelection / 2;
-
-			isProcessing = menuMemory[MemoryKeys.ActiveMenu] == (int)MemoryKeys.MainMenu;
+			quitAnimator.Play("select-no");
+			quitAnimator.Advance(0.0);
 		}
 
-		protected override void ProcessMenu()
+		isQuitMenuActive = false;
+		quitAnimator.Play("hide");
+	}
+
+	protected override void UpdateSelection()
+	{
+		if (isQuitMenuActive)
 		{
-			if (!isQuitMenuActive && Input.IsActionJustPressed("button_pause"))
+			int input = Mathf.Sign(Input.GetAxis("move_left", "move_right"));
+			if ((input > 0 && isQuitSelected) || (input < 0 && !isQuitSelected))
 			{
-				quitAnimator.Play("show");
-				isQuitMenuActive = true;
-				isQuitSelected = false;
-				return;
+				isQuitSelected = !isQuitSelected;
+				quitAnimator.Play(isQuitSelected ? "select-yes" : "select-no");
 			}
 
-			base.ProcessMenu();
-			cursor.Position = cursor.Position.SmoothDamp(options[currentSelection].Position, ref cursorVelocity, CURSOR_SMOOTHING);
+			return;
 		}
 
+		HorizontalSelection = Mathf.Clamp(HorizontalSelection + Mathf.Sign(Input.GetAxis("move_left", "move_right")), 0, 1);
+		VerticalSelection = Mathf.Clamp(VerticalSelection + Mathf.Sign(Input.GetAxis("move_up", "move_down")), 0, 1);
 
-		[Export]
-		private AnimationPlayer quitAnimator;
-		private bool isQuitMenuActive;
-		private bool isQuitSelected;
-		private void CancelQuitMenu()
+		int targetSelection = HorizontalSelection + (VerticalSelection * 2);
+		if (targetSelection != currentSelection)
+		{
+			currentSelection = targetSelection;
+			description.ShowDescription();
+			menuMemory[MemoryKeys.MainMenu] = currentSelection;
+			animator.Play("select");
+			animator.Advance(0.0);
+			AnimateSelection();
+		}
+	}
+
+	protected override void Confirm()
+	{
+		if (isQuitMenuActive)
 		{
 			if (isQuitSelected)
-			{
-				quitAnimator.Play("select-no");
-				quitAnimator.Advance(0.0);
-			}
-
-			isQuitMenuActive = false;
-			quitAnimator.Play("hide");
-		}
-
-		protected override void UpdateSelection()
-		{
-			if (isQuitMenuActive)
-			{
-				int input = Mathf.Sign(Input.GetAxis("move_left", "move_right"));
-				if (input > 0 && isQuitSelected || input < 0 && !isQuitSelected)
-				{
-					isQuitSelected = !isQuitSelected;
-					quitAnimator.Play(isQuitSelected ? "select-yes" : "select-no");
-				}
-
-				return;
-			}
-
-			HorizontalSelection = Mathf.Clamp(HorizontalSelection + Mathf.Sign(Input.GetAxis("move_left", "move_right")), 0, 1);
-			VerticalSelection = Mathf.Clamp(VerticalSelection + Mathf.Sign(Input.GetAxis("move_up", "move_down")), 0, 1);
-
-			int targetSelection = HorizontalSelection + (VerticalSelection * 2);
-			if (targetSelection != currentSelection)
-			{
-				currentSelection = targetSelection;
-				menuMemory[MemoryKeys.MainMenu] = currentSelection;
-				animator.Play("select");
-				animator.Advance(0.0);
-				AnimateSelection();
-			}
-		}
-
-
-		protected override void Confirm()
-		{
-			if (isQuitMenuActive)
-			{
-				if (isQuitSelected)
-					quitAnimator.Play("confirm");
-				else
-					CancelQuitMenu();
-
-				return;
-			}
-
-			//Ignore unimplemented menus.
-			if (currentSelection == 1 || currentSelection == 2) return;
-			animator.Play("confirm");
-		}
-
-
-		protected override void Cancel()
-		{
-			if (isQuitMenuActive)
-			{
+				quitAnimator.Play("confirm");
+			else
 				CancelQuitMenu();
-				return;
-			}
 
-			animator.Play("cancel");
+			return;
 		}
 
-		public override void OpenSubmenu()
-		{
-			if (currentSelection == 0)
-				_submenus[currentSelection].ShowMenu();
-			else if (currentSelection == 3)
-			{
-				FadeBGM(.5f);
-				menuMemory[MemoryKeys.MainMenu] = currentSelection;
-				TransitionManager.QueueSceneChange("res://interface/menu/options/Options.tscn");
-				TransitionManager.StartTransition(new()
-				{
-					color = Colors.Black,
-					inSpeed = .5f,
-				});
-			}
-		}
-
-		private void AnimateSelection() => animator.Play($"select-{currentSelection}");
-
-
-		private void StartQuitTransition()
-		{
-			TransitionManager.instance.Connect(TransitionManager.SignalName.TransitionProcess, new(this, MethodName.QuitGame));
-			TransitionManager.StartTransition(new()
-			{
-				color = Colors.Black,
-				inSpeed = 1f,
-			});
-		}
-		private void QuitGame() => GetTree().Quit();
+		//Ignore unimplemented menus.
+		if (currentSelection == 1 || currentSelection == 2) return;
+		animator.Play("confirm");
 	}
+
+	protected override void Cancel()
+	{
+		if (isQuitMenuActive)
+		{
+			CancelQuitMenu();
+			return;
+		}
+
+		animator.Play("cancel");
+	}
+
+	public override void OpenSubmenu()
+	{
+		if (currentSelection == 0)
+		{
+			_submenus[currentSelection].ShowMenu();
+			return;
+		}
+
+		if (currentSelection != 3)
+			return;
+
+		FadeBGM(.5f);
+		menuMemory[MemoryKeys.MainMenu] = currentSelection;
+		TransitionManager.QueueSceneChange("res://interface/menu/options/Options.tscn");
+		TransitionManager.StartTransition(new()
+		{
+			color = Colors.Black,
+			inSpeed = .5f,
+		});
+	}
+
+	private void AnimateSelection() => animator.Play($"select-{currentSelection}");
+
+	private void StartQuitTransition()
+	{
+		TransitionManager.instance.Connect(TransitionManager.SignalName.TransitionProcess, new(this, MethodName.QuitGame));
+		TransitionManager.StartTransition(new()
+		{
+			color = Colors.Black,
+			inSpeed = 1f,
+		});
+	}
+	private void QuitGame() => GetTree().Quit();
 }
