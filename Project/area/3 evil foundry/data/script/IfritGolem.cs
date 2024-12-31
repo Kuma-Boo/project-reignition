9ace749032bde63f9ca1688fdeb89ee5c5c773ba
Kuma-Boo <kumapauze@gmail.com>
2024-12-31 14:44:50 -0500

    Fix Ifrit Golem freezing after defeat

--- Project/area/3 evil foundry/data/script/IfritGolem.cs
+++ Project/area/3 evil foundry/data/script/IfritGolem.cs
@@ -883,7 +883,7 @@ public partial class IfritGolem : Node3D
 
 		Transform3D t = tank.GlobalTransform;
 		tank.GetParent().RemoveChild(tank);
-		StageSettings.Instance.AddChild(tank);
+		AddChild(tank);
 		tank.GlobalTransform = t;
 
 		if (currentState == GolemState.SpecialAttack)
