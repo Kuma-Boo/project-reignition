e23cdcfbfd2b966d3ac87cb86232715661a0c803
Kuma-Boo <kumapauze@gmail.com>
2025-01-11 18:01:41 -0500

    Start working on SkyRoads

--- Project/area/4 levitated ruin/data/script/SkyRoad.cs
+++ Project/area/4 levitated ruin/data/script/SkyRoad.cs
@@ -1,14 +1,21 @@
 using Godot;
 
+namespace Project.Gameplay.Objects;
+
 public partial class SkyRoad : Node3D
 {
 	[Export] private MeshInstance3D mesh;
 	[Export] private Path3D path;
-	// TODO Add gargoyle [Export] private GargoyleTraveller gargoyle;
+	public Path3D Path => path;
+
+	private readonly StringName PathLengthParameter = "path_length";
+	private readonly StringName PathRatioParameter = "path_ratio";
 
 	public override void _Ready()
 	{
-		mesh.SetInstanceShaderParameter("path_length", path.Curve.GetBakedLength());
-		mesh.SetInstanceShaderParameter("path_ratio", 0.0f);
+		mesh.SetInstanceShaderParameter(PathLengthParameter, path.Curve.GetBakedLength());
+		SetPathRatio(0.0f);
 	}
+
+	public void SetPathRatio(float ratio) => mesh.SetInstanceShaderParameter(PathRatioParameter, ratio);
 }
