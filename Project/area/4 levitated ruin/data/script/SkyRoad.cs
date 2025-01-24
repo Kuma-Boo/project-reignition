a7fce05b34850307dd4a5455eefac5daf0506493
Kuma-Boo <kumapauze@gmail.com>
2025-01-24 17:58:35 -0500

    Implement Gargoyle Path Linking

--- Project/area/4 levitated ruin/data/script/SkyRoad.cs
+++ Project/area/4 levitated ruin/data/script/SkyRoad.cs
@@ -5,17 +5,18 @@ namespace Project.Gameplay.Objects;
 public partial class SkyRoad : Node3D
 {
 	[Export] private MeshInstance3D mesh;
-	[Export] private Path3D path;
-	public Path3D Path => path;
+
+	[Export] private float totalPathLength;
 
 	private readonly StringName PathLengthParameter = "path_length";
 	private readonly StringName PathRatioParameter = "path_ratio";
 
 	public override void _Ready()
 	{
-		mesh.SetInstanceShaderParameter(PathLengthParameter, path.Curve.GetBakedLength());
+		SetPathLength();
 		SetPathRatio(0.0f);
 	}
 
+	public void SetPathLength() => mesh.SetInstanceShaderParameter(PathLengthParameter, totalPathLength);
 	public void SetPathRatio(float ratio) => mesh.SetInstanceShaderParameter(PathRatioParameter, ratio);
 }
--- Project/object/enemy/gargoyle/data/GargoyleSkyroad.cs
+++ Project/object/enemy/gargoyle/data/GargoyleSkyroad.cs
@@ -8,11 +8,21 @@ public partial class GargoyleSkyroad : PathFollow3D
 {
 	private PlayerController Player => StageSettings.Player;
 
+	private SpawnData spawnData;
+
 	[Export] private Node3D root;
 	private Vector3 velocity;
 
 	[Export] private SkyRoad activeRoad;
-	private Path3D CurrentPath => activeRoad.Path;
+	/// <summary> The sequence of paths this Gargoyle will travel (in order). </summary>
+	[Export] private Path3D[] paths;
+	private Path3D CurrentPath => paths[currentPathIndex];
+
+	// Alternative variables because we're working with multiple paths
+	private float traveledDistance; // Distance we finished based on completed paths' baked length 
+	private float totalDistance;
+	private int currentPathIndex;
+
 	private readonly float PositionSmoothing = 0.2f;
 	private readonly float MinDistanceToPlayer = 20.0f;
 	private readonly float BaseMovementSpeed = 20.0f;
@@ -20,7 +30,19 @@ public partial class GargoyleSkyroad : PathFollow3D
 	public override void _Ready()
 	{
 		Visible = false;
+		InitializePathLength();
+
+		spawnData = new(GetParent(), Transform);
+		StageSettings.Instance.ConnectRespawnSignal(this);
+
 		ProcessMode = ProcessModeEnum.Disabled;
+	}
+
+	public void Respawn()
+	{
+		currentPathIndex = 0;
+		velocity = Vector3.Zero;
+		spawnData.Respawn(this);
 	}
 
 	public override void _PhysicsProcess(double _)
@@ -28,39 +50,60 @@ public partial class GargoyleSkyroad : PathFollow3D
 		if (activeRoad == null)
 			return;
 
-		Progress += BaseMovementSpeed * PhysicsManager.physicsDelta;
-
+		// Smoothly move the local position to the correct distance
 		root.Position = root.Position.SmoothDamp(Vector3.Back * 2.0f, ref velocity, PositionSmoothing);
+
+		// Move gargoyle along the path
+		float movementDelta = BaseMovementSpeed * PhysicsManager.physicsDelta;
+		Progress += movementDelta;
+		if (Mathf.IsEqualApprox(ProgressRatio, 1.0f))
+			IncrementPathIndex();
+
+		// Ensure we're a set distance away from the player
 		float playerProgress = CurrentPath.Curve.GetClosestOffset(Player.PathFollower.GlobalPosition - CurrentPath.GlobalPosition);
 		float delta = Progress - playerProgress;
 		if (delta < MinDistanceToPlayer && !Mathf.IsZeroApprox(playerProgress))
 			Progress = playerProgress + MinDistanceToPlayer;
 
-		activeRoad.SetPathRatio(ProgressRatio);
-
-		if (Mathf.IsEqualApprox(ProgressRatio, 1.0f))
-			PlayExitAnimation();
+		activeRoad.SetPathRatio((traveledDistance + Progress) / totalDistance); // Update visuals
 	}
 
-	public void SetSkyRoad()
+	public void IncrementPathIndex()
 	{
-		if (!Visible)
-			PlayEntryAnimation();
+		traveledDistance += CurrentPath.Curve.GetBakedLength(); // Add the current path's length to traveled distance
 
-		GD.Print(activeRoad);
-		if (activeRoad == null)
+		currentPathIndex++;
+		if (currentPathIndex == paths.Length) // Finished all the paths
+		{
+			PlayExitAnimation();
 			return;
+		}
 
-		Vector3 offset = root.GlobalPosition;
-		GetParent().RemoveChild(this);
-		CurrentPath.AddChild(this);
-		Progress = CurrentPath.Curve.GetClosestOffset(offset - CurrentPath.GlobalPosition);
-		root.GlobalPosition = offset;
+		ReparentToPath(); // Reparent to new path and start again
+	}
+
+	// Get the length of all our paths and store for later
+	private void InitializePathLength()
+	{
+		foreach (Path3D path in paths)
+			totalDistance += path.Curve.GetBakedLength();
 	}
 
 	public void Activate()
 	{
-		SetSkyRoad();
+		if (!Visible)
+			PlayEntryAnimation();
+
+		ReparentToPath();
+	}
+
+	private void ReparentToPath()
+	{
+		Vector3 offset = root.GlobalPosition;
+		GetParent().RemoveChild(this);
+		CurrentPath.AddChild(this);
+		Progress = 0;
+		root.GlobalPosition = offset;
 	}
 
 	private void PlayEntryAnimation()
