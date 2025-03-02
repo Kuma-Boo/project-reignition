c6507cda0bc636293b726097edabd5f17bb0387d
Kuma <kumapauze@gmail.com>
2025-03-02 13:52:57 -0500

    Add missing Spring animations (#311)
    * Add Spring Animations
    
    * More Polish
    
    * Update Remaining Springs

---
+++ Project/object/stage/common/resource/script/Spring.cs
@@ -0,0 +1,43 @@
+using Godot;
+
+namespace Project.Gameplay.Objects;
+
+[Tool]
+public partial class Spring : Launcher
+{
+	[Export] private bool startHidden;
+	[Export] private AnimationPlayer animator;
+
+	private readonly StringName LaunchAnim = "launch";
+	private readonly StringName ShowAnim = "show";
+	private readonly StringName HideAnim = "hide";
+
+	protected override void SetUp()
+	{
+		base.SetUp();
+
+		if (Engine.IsEditorHint())
+			return;
+
+		if (startHidden)
+		{
+			StartHidden();
+			StageSettings.Instance.Respawned += StartHidden;
+		}
+	}
+
+	private void StartHidden()
+	{
+		animator.Play(HideAnim);
+		animator.Seek(animator.CurrentAnimationLength, true, true);
+	}
+
+	protected override void LaunchAnimation()
+	{
+		animator.Play(LaunchAnim);
+		base.LaunchAnimation();
+	}
+
+	public void ShowLauncher() => animator.Play(ShowAnim);
+	public void HideLauncher() => animator.Play(HideAnim);
+}
