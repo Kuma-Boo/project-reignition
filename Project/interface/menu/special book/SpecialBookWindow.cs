7f1b1b729f6a7d5c6ab280924e4d859a816f71da
Kuma-Boo <kumapauze@gmail.com>
2025-04-28 10:20:43 -0400

    Refactor Part 1

--- Project/interface/menu/special book/SpecialBookWindow.cs
+++ Project/interface/menu/special book/SpecialBookWindow.cs
@@ -1,30 +1,20 @@
 using Godot;
-using System;
-using Project.Core;
 
 namespace Project.Interface.Menus;
 
 public partial class SpecialBookWindow : Control
 {
+	[Export] private Label numberLabel;
+	[Export] private AnimationPlayer windowAnimator;
+	[Export] private AnimationPlayer windowAnimator2;
 
-    [Export]
-    private AnimationPlayer windowAnimator;
-    [Export]
-    private AnimationPlayer windowAnimator2;
+	public override void _Ready()
+	{
+		// Update the page's number
+		numberLabel.Text = (GetIndex() + 1).ToString();
+	}
 
-    public void Glow()
-    {
-        windowAnimator2.Play("glow");
-    }
-
-    public void Select()
-    {
-        windowAnimator.Play("select");
-    }
-
-    public void Deselect()
-    {
-        windowAnimator.Play("RESET");
-    }
-
+	public void Glow() => windowAnimator2.Play("glow");
+	public void Select() => windowAnimator.Play("select");
+	public void Deselect() => windowAnimator.Play("RESET");
 }
