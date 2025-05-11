ffbe219cc16e2c4703300336ab5b0288dd82de72
Kuma-Boo <kumapauze@gmail.com>
2025-05-11 17:28:28 -0400

    Update fan speeds

--- Project/area/3 evil foundry/data/script/FanHazard.cs
+++ Project/area/3 evil foundry/data/script/FanHazard.cs
@@ -7,28 +7,32 @@ public partial class FanHazard : Hazard
 {
 	/// <summary> How long it takes to make a single rotation, in seconds. </summary>
 	[Export] private float rotationTime;
+	[Export] private float timebreakMultiplier = 0.5f;
 	[Export] private bool playSFX;
 
 	[ExportGroup("Components")]
-	[Export] private NodePath root;
-	private Node3D _root;
-	[Export] private NodePath sfx;
-	private AudioStreamPlayer3D _sfx;
+	[Export] private Node3D root;
+	[Export] private AudioStreamPlayer3D sfx;
 
 	public override void _Ready()
 	{
-		_root = GetNode<Node3D>(root);
-		_sfx = GetNode<AudioStreamPlayer3D>(sfx);
-
 		if (playSFX)
-			_sfx.Play();
+			sfx.Play();
 	}
 
 	public override void _PhysicsProcess(double _)
 	{
 		if (!Mathf.IsZeroApprox(rotationTime))
-			_root.Rotation += Vector3.Back * Mathf.Tau * (1f / rotationTime) * PhysicsManager.physicsDelta;
+			ProcessRotation();
 
 		ProcessCollision();
+	}
+
+	private void ProcessRotation()
+	{
+		float rotationSpeed = 1f / rotationTime;
+		if (Player.Skills.IsTimeBreakActive)
+			rotationSpeed *= timebreakMultiplier;
+		root.Rotation += Vector3.Back * Mathf.Tau * rotationSpeed * PhysicsManager.physicsDelta;
 	}
 }
