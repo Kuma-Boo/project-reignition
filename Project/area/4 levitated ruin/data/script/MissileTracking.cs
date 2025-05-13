6feb16e001d310f6594ce23bb60b25a544205b88
Kuma-Boo <kumapauze@gmail.com>
2025-05-12 20:11:41 -0400

    Adjust missiles

--- Project/area/4 levitated ruin/data/script/MissileTracking.cs
+++ Project/area/4 levitated ruin/data/script/MissileTracking.cs
@@ -10,10 +10,12 @@ public partial class MissileTracking : PathFollow3D
 	[Signal] public delegate void ExplodedEventHandler();
 	[Signal] public delegate void DisabledEventHandler();
 
-	[Export] private float activationDelay;
-	[Export] private float moveSpeed = 30.0f;
-	public void SetSpeed(float value) => moveSpeed = value;
+	[Export] private int explosionPoint = 0;
+	[Export] private float baseSpeed = 20f;
+	[Export] private float activationDelay = 0.1f;
 	[Export] private AnimationPlayer animator;
+
+	private PlayerController Player => StageSettings.Player;
 
 	private SpawnData spawnData;
 	public void UpdateSpawnTransform(Transform3D t)
@@ -26,6 +28,13 @@ public partial class MissileTracking : PathFollow3D
 	private bool isExploded;
 	private float initialProgress;
 
+	private float moveSpeed;
+	private float moveVelocity;
+	private const float TargetRubberbandingDistance = 5f;
+	private const float RubberbandingInterpolationDistance = 10f;
+	private const float RubberbandingSpeed = 20f;
+	private const float MoveSmoothing = 0.2f;
+
 	public override void _Ready()
 	{
 		StageSettings.Instance.Respawned += Respawn;
@@ -37,6 +46,7 @@ public partial class MissileTracking : PathFollow3D
 
 	public void Respawn()
 	{
+		moveSpeed = moveVelocity = 0;
 		Visible = false;
 		isExploded = false;
 		activationTimer = activationDelay;
@@ -72,10 +82,30 @@ public partial class MissileTracking : PathFollow3D
 		if (isExploded)
 			return;
 
+		CalculateSpeed();
 		Progress += moveSpeed * PhysicsManager.physicsDelta;
 
-		if (Mathf.IsEqualApprox(ProgressRatio, 1f))
+		if ((explosionPoint != 0 && Progress > explosionPoint) || Mathf.IsEqualApprox(ProgressRatio, 1f))
 			Explode();
+	}
+
+	private void CalculateSpeed()
+	{
+		float targetSpeed = Mathf.Min(Player.MoveSpeed, Player.Stats.GroundSettings.Speed);
+		targetSpeed = Mathf.Max(targetSpeed, baseSpeed);
+		targetSpeed += CalculateRubberbandingSpeed();
+		moveSpeed = ExtensionMethods.SmoothDamp(moveSpeed, targetSpeed, ref moveVelocity, MoveSmoothing);
+	}
+
+	private float CalculateRubberbandingSpeed()
+	{
+		float playerProgress = Player.PathFollower.Progress;
+		float missileProgress = Player.PathFollower.GetProgress(GlobalPosition);
+		float targetRubberbandingSpeed = playerProgress - TargetRubberbandingDistance - missileProgress;
+		targetRubberbandingSpeed /= RubberbandingInterpolationDistance;
+		targetRubberbandingSpeed = Mathf.Clamp(targetRubberbandingSpeed, 0f, 1f);
+		targetRubberbandingSpeed *= RubberbandingSpeed;
+		return targetRubberbandingSpeed;
 	}
 
 	private void Explode()
