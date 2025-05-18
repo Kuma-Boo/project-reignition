3893fd175779a75918712b3dbe53da640b6e3939
Kuma-Boo <kumapauze@gmail.com>
2025-05-18 10:26:27 -0400

    Add floating platforms

--- Project/object/trigger/modules/PlatformTrigger.cs
+++ Project/object/trigger/modules/PlatformTrigger.cs
@@ -22,6 +22,15 @@ public partial class PlatformTrigger : Node3D
 			properties.Add(ExtensionMethods.CreateProperty("Falling Platform Settings/Shake Length", Variant.Type.Float, PropertyHint.Range, "0, 10"));
 		}
 
+		properties.Add(ExtensionMethods.CreateProperty("Floating Platform Settings/Enabled", Variant.Type.Bool));
+		if (isFloatingBehaviorEnabled)
+		{
+			properties.Add(ExtensionMethods.CreateProperty("Floating Platform Settings/Height Range", Variant.Type.Float, PropertyHint.Range, "0,5,0.1"));
+			properties.Add(ExtensionMethods.CreateProperty("Floating Platform Settings/Force Multiplier", Variant.Type.Float, PropertyHint.Range, "0,2,0.1"));
+			properties.Add(ExtensionMethods.CreateProperty("Floating Platform Settings/Initial Force", Variant.Type.Float));
+			properties.Add(ExtensionMethods.CreateProperty("Floating Platform Settings/Initial Height", Variant.Type.Float, PropertyHint.Range, "-1,1,0.1"));
+		}
+
 		return properties;
 	}
 
@@ -47,6 +56,21 @@ public partial class PlatformTrigger : Node3D
 				isFloatingBehaviorEnabled = (bool)value;
 				NotifyPropertyListChanged();
 				break;
+			case "Floating Platform Settings/Height Range":
+				floatingHeightRange = (float)value;
+				break;
+			case "Floating Platform Settings/Force Multiplier":
+				floatingForceMultiplier = (float)value;
+				break;
+			case "Floating Platform Settings/Return Strength":
+				buoyancyStrength = (float)value;
+				break;
+			case "Floating Platform Settings/Initial Force":
+				currentBuoyancyForce = (float)value;
+				break;
+			case "Floating Platform Settings/Initial Height":
+				floatingInitialHeightRatio = (float)value;
+				break;
 			default:
 				return false;
 		}
@@ -69,6 +93,16 @@ public partial class PlatformTrigger : Node3D
 
 			case "Floating Platform Settings/Enabled":
 				return isFloatingBehaviorEnabled;
+			case "Floating Platform Settings/Height Range":
+				return floatingHeightRange;
+			case "Floating Platform Settings/Force Multiplier":
+				return floatingForceMultiplier;
+			case "Floating Platform Settings/Return Strength":
+				return buoyancyStrength;
+			case "Floating Platform Settings/Initial Force":
+				return currentBuoyancyForce;
+			case "Floating Platform Settings/Initial Height":
+				return floatingInitialHeightRatio;
 		}
 
 		return base._Get(property);
@@ -78,6 +112,7 @@ public partial class PlatformTrigger : Node3D
 	[Signal]
 	public delegate void PlatformInteractedEventHandler();
 
+	// Falling platform variables
 	/// <summary> Animator to handle falling platform behaviour. </summary>
 	private AnimationPlayer fallingPlatformAnimator;
 	private NodePath fallingPlatformAnimatorPath;
@@ -89,25 +124,40 @@ public partial class PlatformTrigger : Node3D
 	private float shakeLength;
 	/// <summary> Keeps track of the platform's position from the previous frame. </summary>
 	private Vector3 previousPosition;
-
-	/// <summary> Will this platform bob up and down when the player jumps on it? </summary>
-	private bool isFloatingBehaviorEnabled;
-	/// <summary> Determines the smoothing of how the platform bobs. </summary>
-	private Curve2D buoyancyCurve;
-	/// <summary> Determines how much force it takes to move the platform. </summary>
-	private float massRatio;
-
-	/// <summary> Bump this up to allow the player to go flying when jumping off. </summary>
-	[Export] private float playerJumpInfluenceMultiplier = 1f;
-	[Export(PropertyHint.Range, "0,1,.1,or_greater")] private float maxJumpMovementAmount;
-
-	// Runtime data
 	/// <summary> Timer to keep track of shaking status. </summary>
 	private float shakeTimer;
 	/// <summary> Is the platform about to fall? </summary>
 	private bool isPlatformShaking;
 
-	[ExportGroup("Components")]
+	// Floating platform variables
+	/// <summary> Will this platform bob up and down when the player jumps on it? </summary>
+	private bool isFloatingBehaviorEnabled;
+	/// <summary> Determines the maximum (and minimum) height the platform can bob to. </summary>
+	private float floatingHeightRange = 1f;
+	/// <summary> Determines how much force it takes to move the platform. </summary>
+	private float floatingForceMultiplier = 1f;
+	/// <summary> Determines how strongly the platform bounces back to neutral. </summary>
+	private float buoyancyStrength = 5f;
+	/// <summary>
+	/// Keeps track of the floating platform's current velocity.
+	/// Initialize to a non-zero value if you want the platform to start in a bobbing state.
+	/// </summary>
+	private float currentBuoyancyForce;
+	/// <summary> The platform's initial Y-Position. </summary>
+	private float floatingReferenceHeight;
+	/// <summary> The platform's starting Y-Position, used to offset different platforms from each other. </summary>
+	private float floatingInitialHeightRatio;
+	/// <summary> Has the force from the incoming player been added to the platform? </summary>
+	private bool appliedFloatingForce;
+	private readonly float BuoyancySmoothingRatio = 0.4f;
+	private readonly float BaseFloatingForceMultiplier = 0.5f;
+	private readonly float BasePlayerFloatingForce = 5f;
+
+	/// <summary> Bump this up to allow the player to go flying when jumping off. </summary>
+	[Export] private float playerJumpInfluenceMultiplier = 1f;
+	[Export(PropertyHint.Range, "0,1,.1,or_greater")] private float maxJumpMovementAmount;
+
+	[ExportSubgroup("Components")]
 	/// <summary> Assign this to enable moving the player with the platform. </summary>
 	[Export] private Node3D floorCalculationRoot;
 	/// <summary> Reference to the "floor" collider. </summary>
@@ -133,12 +183,19 @@ public partial class PlatformTrigger : Node3D
 			if (autoShake) // Falling behaviour is enabled, connect signal.
 				Connect(SignalName.PlatformInteracted, new Callable(this, MethodName.StartShaking));
 		}
+
+		if (isFloatingBehaviorEnabled)
+		{
+			floatingReferenceHeight = GlobalPosition.Y;
+			GlobalPosition += Vector3.Up * floatingInitialHeightRatio * floatingHeightRange;
+		}
 	}
 
 	public override void _PhysicsProcess(double _)
 	{
 		if (Engine.IsEditorHint()) return;
 
+		UpdateFloatingPlatform();
 		UpdatePlatform();
 	}
 
@@ -163,6 +220,50 @@ public partial class PlatformTrigger : Node3D
 
 		if (floorCalculationRoot != null)
 			CallDeferred(MethodName.SyncPlayerMovement);
+	}
+
+	private void UpdateFloatingPlatform()
+	{
+		if (!isFloatingBehaviorEnabled || Mathf.IsZeroApprox(floatingHeightRange))
+			return;
+
+		if (isInteractingWithPlayer)
+			ProcessIncomingFloatForce();
+
+		float deltaForce = (floatingReferenceHeight - GlobalPosition.Y) / floatingHeightRange;
+		if (Mathf.Abs(deltaForce) > 1.0f - BuoyancySmoothingRatio) // Smooth out bottoms
+		{
+			float smoothingRatio = (1.0f - Mathf.Abs(deltaForce)) / BuoyancySmoothingRatio;
+			if (deltaForce > 0 && currentBuoyancyForce < 0) // Bounce back faster when hitting the bottom
+			{
+				deltaForce += (1.0f - smoothingRatio) * 10f;
+				currentBuoyancyForce *= 0.8f;
+			}
+		}
+
+		if (GlobalPosition.Y > floatingReferenceHeight + floatingHeightRange) // Add gravity when the platform is too high up
+			currentBuoyancyForce -= Runtime.Gravity * PhysicsManager.physicsDelta;
+
+		currentBuoyancyForce += buoyancyStrength * deltaForce * PhysicsManager.physicsDelta;
+
+		// Move platform, but prevent it from going too low
+		GlobalPosition += Vector3.Up * currentBuoyancyForce * PhysicsManager.physicsDelta;
+	}
+
+	private void ProcessIncomingFloatForce()
+	{
+		if (appliedFloatingForce)
+			return;
+
+		if (!IsPlayerOnPlatform()) // Force isn't going to be applied -- don't bother calculating anything
+			return;
+
+		float targetForce = Mathf.Min(Player.VerticalSpeed, 0f);
+		if (Player.IsOnGround) // Apply a consistent force if the player walks onto a platform
+			targetForce = BasePlayerFloatingForce;
+
+		currentBuoyancyForce = -Mathf.Abs(targetForce) * floatingForceMultiplier * BaseFloatingForceMultiplier;
+		appliedFloatingForce = true;
 	}
 
 	/// <summary> Make the platform start shaking. This can also be called from a signal. </summary>
@@ -224,10 +325,7 @@ public partial class PlatformTrigger : Node3D
 			return;
 		}
 
-		float checkLength = Mathf.Abs(Player.CenterPosition.Y - floorCalculationRoot.GlobalPosition.Y) + (Player.CollisionSize.Y * 2.0f);
-		KinematicCollision3D collision = Player.MoveAndCollide(-Player.UpDirection * checkLength, true);
-
-		if (collision == null || (Node3D)collision.GetCollider() != parentCollider) // Player is not on the platform
+		if (!IsPlayerOnPlatform())
 			return;
 
 		playerInfluence = 1f; // Set player influence to 1 for when we leave
@@ -235,11 +333,29 @@ public partial class PlatformTrigger : Node3D
 		Player.GlobalTranslate(Vector3.Up * (floorCalculationRoot.GlobalPosition.Y - Player.GlobalPosition.Y));
 	}
 
+	/// <summary> Checks whether the player is currently standing on top of the platform. </summary>
+	private bool IsPlayerOnPlatform()
+	{
+		float checkLength = Mathf.Abs(Player.CenterPosition.Y - floorCalculationRoot.GlobalPosition.Y) + (Player.CollisionSize.Y * 2.0f);
+		KinematicCollision3D collision = Player.MoveAndCollide(-Player.UpDirection * checkLength, true);
+
+		if (collision == null || (Node3D)collision.GetCollider() != parentCollider) // Player is not on the platform
+			return false;
+
+		return true;
+	}
+
 	public void OnEntered(Area3D a)
 	{
 		if (!a.IsInGroup("player detection")) return;
 		isInteractingWithPlayer = true;
 		UpdatePlatform();
+
+		if (isFloatingBehaviorEnabled)
+		{
+			appliedFloatingForce = false;
+			ProcessIncomingFloatForce();
+		}
 	}
 
 	public void OnExited(Area3D a)
