5b981f56df9928a57cfeba8b39f0b08e528af22d
Kuma-Boo <kumapauze@gmail.com>
2025-05-22 17:19:55 -0400

    Move PathTraveller Logic

--- Project/object/stage/common/resource/script/PathTraveller.cs
+++ Project/object/stage/common/resource/script/PathTraveller.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Project.Core;
 
 namespace Project.Gameplay.Objects;
 
@@ -18,10 +19,8 @@ public partial class PathTraveller : Node3D
 	/// <summary> How fast to move. </summary>
 	[ExportGroup("Settings")]
 	[Export] private float maxSpeed;
-	public float MaxSpeed => maxSpeed;
 	/// <summary> How fast to turn. </summary>
 	[Export] private float turnSpeed;
-	public float TurnSpeed => turnSpeed;
 
 	/// <summary> Allow object to move vertically? </summary>
 	[Export] private bool isVerticalMovementDisabled;
@@ -53,9 +52,23 @@ public partial class PathTraveller : Node3D
 	/// <summary> Reference to the animator. </summary>
 	[Export] private AnimationPlayer animator;
 
+	/// <summary> How fast is the object currently moving? </summary>
+	public float CurrentSpeed { get; protected set; }
+	/// <summary> How much is the object currently turning? </summary>
+	private Vector2 currentTurnAmount;
+	// Values for smooth damp
+	private float speedVelocity;
+	private Vector2 turnVelocity;
+
 	private float startingProgress;
 	private Vector3 startingOffset;
 	private SpawnData spawnData;
+	private float HorizontalTurnSmoothing => Bounds.X - CollisionSmoothingDistance;
+	private float VerticalTurnSmoothing => Bounds.Y - CollisionSmoothingDistance;
+	/// <summary> At what distance should inputs start being smoothed? </summary>
+	private readonly float CollisionSmoothingDistance = 1f;
+	private readonly float SpeedSmoothing = .5f;
+	private readonly float TurnSmoothing = .25f;
 
 	private PlayerController Player => StageSettings.Player;
 
@@ -80,29 +93,6 @@ public partial class PathTraveller : Node3D
 
 		StageSettings.Instance.Respawned += Respawn;
 		Respawn();
-	}
-
-	public void UpdateAnimation(Vector2 turnVector, float currentSpeed)
-	{
-		// Update animations
-		if (root != null) // Update visual rotations
-		{
-			float turnAmount = (turnVector.X / turnSpeed) - (Player.PathFollower.DeltaAngle * 5.0f);
-			float tiltAmount = Mathf.DegToRad(rotationAmount) * tiltRatio * turnAmount;
-			root.Rotation = Vector3.Zero;
-
-			root.RotateX(Mathf.DegToRad(rotationAmount) * (turnVector.Y / turnSpeed));
-			if (localRoot != null)
-				localRoot.Rotation = new(0, 0, tiltAmount);
-			else
-				root.RotateZ(tiltAmount);
-			root.RotateY(-Mathf.DegToRad(rotationAmount) * turnAmount);
-		}
-
-		if (animator != null) // Update animation speeds
-			animator.SpeedScale = 1.0f + (currentSpeed / maxSpeed * 1.5f);
-		Player.Animator.UpdateBalancing(Player.Controller.InputAxis.X - (Player.PathFollower.DeltaAngle * 20.0f));
-		Player.Animator.UpdateBalanceSpeed(1f + Player.Stats.GroundSettings.GetSpeedRatio(currentSpeed), 0f);
 	}
 
 	private void Respawn()
@@ -131,6 +121,106 @@ public partial class PathTraveller : Node3D
 	{
 		if (animator.HasAnimation("despawn"))
 			animator.Play("despawn");
+	}
+
+
+	public void ProcessPathTraveller()
+	{
+		CalculateMovement();
+		if (AutosetBounds)
+		{
+			// Reset travel bounds on both sides, then update each side individually
+			SetHorizontalBounds(Mathf.Inf);
+			UpdateCollisions(1);
+			UpdateCollisions(-1);
+		}
+
+		ApplyMovement();
+		UpdateAnimation();
+	}
+
+	/// <summary> Check for walls. </summary>
+	private void UpdateCollisions(int direction)
+	{
+		float pathTravellerCollisionSize = Player.CollisionSize.X;
+		float castDistance = pathTravellerCollisionSize + CollisionSmoothingDistance;
+		if (Mathf.Sign(currentTurnAmount.X) == direction)
+			castDistance += Mathf.Abs(currentTurnAmount.X * PhysicsManager.physicsDelta);
+
+		Vector3 castVector = Player.Left() * direction * castDistance;
+		RaycastHit wallCast = Player.CastRay(Player.GlobalPosition, castVector, Runtime.Instance.environmentMask);
+		DebugManager.DrawRay(Player.GlobalPosition, castVector, wallCast ? Colors.Green : Colors.White);
+		if (wallCast)
+			SetHorizontalBounds(Mathf.Abs(PathFollower.HOffset) + (wallCast.distance - pathTravellerCollisionSize));
+	}
+
+	/// <summary> Handles player input. </summary>
+	private void CalculateMovement()
+	{
+		Vector2 inputVector = Player.Controller.InputAxis * turnSpeed;
+		if (IsVerticalMovementDisabled) // Ignore vertical input
+			inputVector.Y = 0;
+
+		// Smooth out edges
+		bool isSmoothingHorizontal = Mathf.Abs(PathFollower.HOffset) > HorizontalTurnSmoothing &&
+			Mathf.Sign(inputVector.X) != Mathf.Sign(PathFollower.HOffset);
+		bool isSmoothingVertical = Mathf.Abs(PathFollower.VOffset) > VerticalTurnSmoothing &&
+			Mathf.Sign(inputVector.Y) != Mathf.Sign(PathFollower.VOffset);
+
+		if (isSmoothingHorizontal)
+			inputVector.X *= 1.0f - ((Mathf.Abs(PathFollower.HOffset) - HorizontalTurnSmoothing) / (Bounds.X - HorizontalTurnSmoothing));
+
+		if (isSmoothingVertical)
+			inputVector.Y *= 1.0f - ((Mathf.Abs(PathFollower.VOffset) - VerticalTurnSmoothing) / (Bounds.Y - VerticalTurnSmoothing));
+
+		currentTurnAmount = currentTurnAmount.SmoothDamp(inputVector, ref turnVelocity, TurnSmoothing);
+		CurrentSpeed = ExtensionMethods.SmoothDamp(CurrentSpeed, maxSpeed, ref speedVelocity, SpeedSmoothing);
+	}
+
+	/// <summary> Override this if you want specific control over the PathFollower's speed. </summary>
+	protected virtual float CalculateMaxSpeed() => maxSpeed;
+
+	private void UpdateAnimation()
+	{
+		// Update animations
+		if (root != null) // Update visual rotations
+		{
+			float turnAmount = (currentTurnAmount.X / turnSpeed) - (Player.PathFollower.DeltaAngle * 5.0f);
+			float tiltAmount = Mathf.DegToRad(rotationAmount) * tiltRatio * turnAmount;
+			root.Rotation = Vector3.Zero;
+
+			root.RotateX(Mathf.DegToRad(rotationAmount) * (currentTurnAmount.Y / turnSpeed));
+			if (localRoot != null)
+				localRoot.Rotation = new(0, 0, tiltAmount);
+			else
+				root.RotateZ(tiltAmount);
+			root.RotateY(-Mathf.DegToRad(rotationAmount) * turnAmount);
+		}
+
+		if (animator != null) // Update animation speeds
+			animator.SpeedScale = 1.0f + (CurrentSpeed / maxSpeed * 1.5f);
+	}
+
+	private void ApplyMovement()
+	{
+		// Update path follower
+		PathFollower.Progress += CurrentSpeed * PhysicsManager.physicsDelta;
+		// Add offsets
+		PathFollower.HOffset -= currentTurnAmount.X * PhysicsManager.physicsDelta;
+		PathFollower.VOffset -= currentTurnAmount.Y * PhysicsManager.physicsDelta;
+		// Clamp offsets
+		PathFollower.HOffset = Mathf.Clamp(PathFollower.HOffset, -Bounds.X, Bounds.X);
+		PathFollower.VOffset = Mathf.Clamp(PathFollower.VOffset, -Bounds.Y, Bounds.Y);
+
+		// Sync transforms
+		GlobalTransform = PathFollower.GlobalTransform;
+	}
+
+	/// <summary> Kills all speed and turning. </summary>
+	public void StopMovement()
+	{
+		CurrentSpeed = speedVelocity = 0;
+		currentTurnAmount = turnVelocity = Vector2.Zero;
 	}
 
 	/// <summary> Call this from a trigger. </summary>
