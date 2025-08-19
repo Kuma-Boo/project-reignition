c9e8eaab61dcc23dc0a96fec74c0d65892952657
Chris Margroff <sora3087@gmail.com>
2025-08-19 10:25:23 -0400

    make vkb trigger a global class and add scanning for different parent types

--- Project/interface/virtual-keyboard/components/VKBTrigger.cs
+++ Project/interface/virtual-keyboard/components/VKBTrigger.cs
@@ -2,19 +2,37 @@ using Godot;
 
 namespace Project.Interface.VirtualKeyboard;
 
+[GlobalClass]
 public partial class VKBTrigger : Node
 {
 	private Control _target;
 	private VirtualKeyboard _vkb;
 	public override void _Ready()
 	{
-		_target = GetParent<LineEdit>();
+		_target = GetParent<Control>();
+		if (_target is LineEdit lineEdit)
+		{
+			lineEdit.VirtualKeyboardEnabled = false;
+			lineEdit.VirtualKeyboardShowOnFocus = false;
+		}
+		else if (_target is TextEdit textEdit)
+		{
+			textEdit.VirtualKeyboardEnabled = false;
+			textEdit.VirtualKeyboardShowOnFocus = false;
+		}
 		_target.Connect("focus_entered", Callable.From(this.Parent_Focused));
 
 		_vkb = GetNode<VirtualKeyboard>("/root/VirtualKeyboard");
 	}
 	private void Parent_Focused()
 	{
-		_vkb.ShowKeyboard(_target);
+		if (_vkb is not null)
+			_vkb.ShowKeyboard(_target);
 	}
+}
+
+public interface ICanTriggerNativeVKB
+{
+	public bool VirtualKeyboardEnabled { get; set; }
+	public bool VirtualKeyboardShowOnFocus { get; set; }
 }
