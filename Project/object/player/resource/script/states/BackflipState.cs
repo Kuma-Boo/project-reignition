b6fd4b0dcd7a226dc1820797e288001f7885407c
Kuma <kumapauze@gmail.com>
2025-08-23 15:35:39 -0400

    Allow double jump from backflip

--- Project/object/player/resource/script/states/BackflipState.cs
+++ Project/object/player/resource/script/states/BackflipState.cs
@@ -67,6 +67,13 @@ public partial class BackflipState : PlayerState
 		if (Player.Controller.IsJumpBufferActive)
 		{
 			Player.Controller.ResetJumpBuffer();
+
+			if (Player.CanDoubleJump && SaveManager.ActiveSkillRing.IsSkillEquipped(SkillKey.DoubleJump)) // Start a double jump
+			{
+				Player.StartDoubleJump();
+				return null;
+			}
+
 			if (SaveManager.Config.useStompJumpButtonMode)
 				return stompState;
 
